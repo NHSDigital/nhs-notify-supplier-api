daab6a6354f221f4ae38706f1c696a7f8961ab93
Ross Buggins <ross.buggins@nhs.net>
2025-07-02 15:23:31 +0000

    remove sdk

--- sdk/python/openapi_client/api/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.batch_api import BatchApi
-from openapi_client.api.data_api import DataApi
-from openapi_client.api.letter_api import LetterApi
-from openapi_client.api.mi_api import MiApi
-from openapi_client.api.whitemail_api import WhitemailApi
-
