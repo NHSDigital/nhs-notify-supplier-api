93cd113d063fe80ff1179b7a5bd019654ae98529
Ross Buggins <ross.buggins@nhs.net>
2025-07-03 14:09:39 +0000

    Trying the npm publish.

--- sdk/generate-ts.sh
+++ sdk/generate-ts.sh
@@ -7,7 +7,7 @@ docker run \
  -v ${PWD}:/local openapitools/openapi-generator-cli generate \
  -i /local/specification/api/notify-supplier.yml \
  -g typescript \
- --additional-properties=npmRepository=https://github.com/NHSDigital/nhs-notify-supplier-api.git,npmName=nhsnotifysupplier,npmVersion=0.1,licenseName=MIT \
+ --additional-properties=npmRepository=https://github.com/NHSDigital/nhs-notify-supplier-api.git,npmName=nhsnotifysupplier,npmVersion=0.1.0,licenseName=MIT \
  -o /local/sdk/typescript \
  --skip-validate-spec
 
