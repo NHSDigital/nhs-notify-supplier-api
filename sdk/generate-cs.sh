ccd8f000d01025593acd3361e81a57fce853c910
Ross Buggins <ross.buggins@nhs.net>
2025-07-04 07:32:53 +0000

    Added dummy responses in for every API method so the csharp generated code is correct.

--- sdk/generate-cs.sh
+++ sdk/generate-cs.sh
@@ -1,25 +1,41 @@
 #!/bin/sh
 # run this from directory above, or with npm run generate:ts from parent directory.
 
-mkdir -p sdk/csharp
 
-VERSION="$(cat .version)"
-echo $VERSION
+generate () {
+  set -x
+  docker run \
+  --rm \
+  --user $(id -u) \
+  -v ${PWD}:/local \
+  -e VERSION="$VERSION" \
+  openapitools/openapi-generator-cli \
+  generate \
+  -i /local/specification/api/notify-supplier.yml \
+  -g csharp \
+  --additional-properties="packageName=nhsnotifysupplier,packageVersion=$SHORT_VERSION,licenseId=MIT,targetFramework=net8.0" \
+  -o /local/sdk/csharp \
+  --skip-validate-spec
+  set +x
+}
 
-set -x
-docker run \
---rm \
---user $(id -u) \
- -v ${PWD}:/local \
- -e VERSION="$VERSION" \
- openapitools/openapi-generator-cli \
- generate \
- -i /local/specification/api/notify-supplier.yml \
- -g csharp \
- --additional-properties="packageName=nhsnotifysupplier,packageVersion=$VERSION,licenseId=MIT,targetFramework=net8.0" \
- -o /local/sdk/csharp \
- --skip-validate-spec
-set +x
+build () {
+  dotnet build sdk/csharp/src/nhsnotifysupplier
+}
+
+prepare(){
+  mkdir -p sdk/csharp
+
+  VERSION="$(cat .version)"
+  echo $VERSION
+
+  SHORT_VERSION="$(echo $VERSION | rev | cut -d"." -f2-  | rev)"
+  echo $SHORT_VERSION
+}
+
+prepare
+generate
+build
 
 # sed -i -e 's|https://github.com/GIT_USER_ID/GIT_REPO_ID.git|https://github.com/NHSDigital/nhs-notify-supplier-api.git|g'  ./sdk/typescript/package.json
 
