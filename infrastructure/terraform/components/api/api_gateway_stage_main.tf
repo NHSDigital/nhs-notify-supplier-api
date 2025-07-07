62ec78a866a417e167d45cad9dc2c96b2f4c777b
aidenvaines-bjss <aiden.vaines@bjss.com>
2025-07-07 16:40:38 +0100

    CCM-10927 Fix stage name

--- infrastructure/terraform/components/api/api_gateway_stage_main.tf
+++ infrastructure/terraform/components/api/api_gateway_stage_main.tf
@@ -1,5 +1,5 @@
 resource "aws_api_gateway_stage" "main" {
-  stage_name    = var.environment
+  stage_name    = "main"
   description   = "Templates API stage ${var.environment}"
   rest_api_id   = aws_api_gateway_rest_api.main.id
   deployment_id = aws_api_gateway_deployment.main.id
