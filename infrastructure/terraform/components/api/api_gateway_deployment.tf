b52c5bd05289ec9c84a13c6835ca2dc2cecd691e
aidenvaines-bjss <aiden.vaines@bjss.com>
2025-07-07 16:51:55 +0100

    CCM-10922 Fix default stage name

--- infrastructure/terraform/components/api/api_gateway_deployment.tf
+++ infrastructure/terraform/components/api/api_gateway_deployment.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_deployment" "main" {
   rest_api_id = aws_api_gateway_rest_api.main.id
-  description = "Suppliersv UI API deployment"
+  description = "Suppliers UI API deployment"
 
   triggers = {
     openapi_hash = sha1(jsonencode(local.openapi_spec)),
