eedf32c4fc270430152848a3e90914d11d648bd7
sidnhs <siddharth.nair1@nhs.net>
2025-07-08 12:04:41 +0100

    CCM-11007: Fixing log destination for Splunk forwarding (#58)

--- infrastructure/terraform/components/api/locals.tf
+++ infrastructure/terraform/components/api/locals.tf
@@ -8,5 +8,5 @@ locals {
     HELLO_WORLD_LAMBDA_ARN  = module.hello_world.function_arn
   })
 
-  destination_arn = "arn:aws:logs:${var.region}:${var.shared_infra_account_id}:destination:nhs-${var.environment}-obs-firehose-logs"
+  destination_arn = "arn:aws:logs:${var.region}:${var.shared_infra_account_id}:destination:nhs-main-obs-firehose-logs"
 }
