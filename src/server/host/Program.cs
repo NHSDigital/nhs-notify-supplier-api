27f84d6f4db0cb9910d23368a9784a5edc362526
Ross Buggins <ross.buggins@nhs.net>
2025-07-08 14:01:35 +0100

    ....

--- src/server/host/Program.cs
+++ src/server/host/Program.cs
@@ -11,18 +11,13 @@ builder.Services.AddSuppliersApi(
   builder.Configuration,
   suppliersApiBuilder =>
   {
-<<<<<<< HEAD
+
     suppliersApiBuilder
       .FromAssemblies()
       .ConfigureOptions(suppliersOptions =>
       {
       });
-=======
-    suppliersApiBuilder.FromAssemblies();
-    suppliersApiBuilder.ConfigureOptions(suppliersOptions =>
-    {
-    });
->>>>>>> origin/main
+
   });
 
 var app = builder.Build();
