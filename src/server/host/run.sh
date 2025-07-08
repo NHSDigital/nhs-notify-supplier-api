1a2f9c93931b1b1eef951135fccb6d26574aa499
Ross Buggins <ross.buggins@nhs.net>
2025-07-08 12:35:15 +0100

    run.sh assembly names.

--- src/server/host/run.sh
+++ src/server/host/run.sh
@@ -2,6 +2,6 @@
 
 SuppliersApi__Letters=true \
 SuppliersApi__Data=true \
-SuppliersApi__Assemblies__0=letter \
-SuppliersApi__Assemblies__1=data \
+SuppliersApi__Assemblies__0="nhs.notify.suppliers.api.letter" \
+SuppliersApi__Assemblies__1="nhs.notify.suppliers.api.data" \
 dotnet nhs.notify.suppliers.api.host.dll
