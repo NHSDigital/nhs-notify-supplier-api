af49022c7732dd2b5a631e6153d5ab83b93f72c0
Ross Buggins (NHS) <ross.buggins@nhs.net>
2025-07-09 10:37:48 +0100

    Robu6/libs nuget example (#68)
    * Adding libs examples.
    
    * Add libs action to build stage.
    
    * correct typo
    
    * run.sh assembly names.
    
    * secrets.
    
    * CCM-11007: Fixing log destination for Splunk forwarding (#58)
    
    * Robu6/libs nuget example (#57)
    
    * Adding libs examples.
    
    * Add libs action to build stage.
    
    * correct typo
    
    * run.sh assembly names.
    
    * rebased to main.
    
    * ....
    
    * <<< left in
    
    * Adding other example poc libs.
    
    * timeouts of actions increased to 10m from 3m.
    
    * version copy to abstract folder.
    
    * version file not in libs on github.
    
    * find .version and print on pub.
    
    * debugging in build.
    
    * include .version in libs - artifact hidden files.
    
    * correct data nuget package name.
    
    * Trying the docker actions.
    
    * Trying the docker actions.
    
    * Trying the docker actions.
    
    * Trying the docker actions.
    
    * Adding docker publish from artefact
    
    ---------
    
    Co-authored-by: sidnhs <siddharth.nair1@nhs.net>

--- src/server/host/run.sh
+++ src/server/host/run.sh
@@ -4,4 +4,5 @@ SuppliersApi__Letters=true \
 SuppliersApi__Data=true \
 SuppliersApi__Assemblies__0="nhs.notify.suppliers.api.letter" \
 SuppliersApi__Assemblies__1="nhs.notify.suppliers.api.data" \
+ASPNETCORE_ENVIRONMENT="Development" \
 dotnet nhs.notify.suppliers.api.host.dll
