2b3ac47a4b4ed0a15b73dd18258433d9c9be428a
mark.slowey1 <mark.slowey1@nhs.net>
2025-08-18 16:29:20 +0100

    CCM-11586: store ref not indexed

--- infrastructure/terraform/components/api/s3_bucket_truststore.tf
+++ infrastructure/terraform/components/api/s3_bucket_truststore.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "truststore" {
 }
 
 resource "aws_s3_bucket_ownership_controls" "truststore" {
-  bucket = aws_s3_bucket.truststore[0].id
+  bucket = aws_s3_bucket.truststore.id
 
   rule {
     object_ownership = "BucketOwnerPreferred"
@@ -12,7 +12,7 @@ resource "aws_s3_bucket_ownership_controls" "truststore" {
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "truststore" {
-  bucket = aws_s3_bucket.truststore[0].id
+  bucket = aws_s3_bucket.truststore.id
 
   rule {
     apply_server_side_encryption_by_default {
@@ -24,7 +24,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "truststore" {
 }
 
 resource "aws_s3_bucket_versioning" "truststore" {
-  bucket = aws_s3_bucket.truststore[0].id
+  bucket = aws_s3_bucket.truststore.id
 
   versioning_configuration {
     status = "Enabled"
@@ -36,7 +36,7 @@ resource "aws_s3_bucket_public_access_block" "truststore" {
     aws_s3_bucket_policy.truststore
   ]
 
-  bucket = aws_s3_bucket.truststore[0].id
+  bucket = aws_s3_bucket.truststore.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -45,7 +45,7 @@ resource "aws_s3_bucket_public_access_block" "truststore" {
 }
 
 resource "aws_s3_bucket_logging" "truststore" {
-  bucket = aws_s3_bucket.truststore[0].id
+  bucket = aws_s3_bucket.truststore.id
 
   target_bucket = var.truststore_s3_bucket_config.bucket_logs_bucket_name
   target_prefix = "truststore/${aws_s3_bucket.truststore[0].bucket}/"
@@ -54,9 +54,9 @@ resource "aws_s3_bucket_logging" "truststore" {
 # If Environment is to be Manually configured, need to create a placeholder truststore file for mtls
 resource "aws_s3_object" "placeholder_truststore" {
   count   = var.manually_configure_mtls_truststore ? 1 : 0
-  bucket  = aws_s3_bucket.truststore[0].bucket
+  bucket  = aws_s3_bucket.truststore.bucket
   key     = "truststore.pem"
-  content = tls_self_signed_cert.placeholder_cert[0].cert_pem
+  content = tls_self_signed_cert.placeholder_cert.cert_pem
 
   depends_on = [
     aws_s3_bucket_versioning.truststore
@@ -74,7 +74,7 @@ resource "aws_s3_object" "placeholder_truststore" {
 # We don't want to ignore content in nonprod, but we do for prod as we will manually update certs and not via ssl module
 resource "aws_s3_object" "placeholder_truststore_nonprod" {
   count   = var.manually_configure_mtls_truststore ? 0 : 1
-  bucket  = aws_s3_bucket.truststore[0].bucket
+  bucket  = aws_s3_bucket.truststore.bucket
   key     = "truststore.pem"
   content = module.supplier_ssl.cacert_pem
 
