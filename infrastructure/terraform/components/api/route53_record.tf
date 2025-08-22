f3b7c749125d9b4dda06e0032cd56b1af5e970b1
mark.slowey1 <mark.slowey1@nhs.net>
2025-08-22 14:56:17 +0100

    CCM-11586: is it the dynamic names

--- infrastructure/terraform/components/api/route53_record.tf
+++ infrastructure/terraform/components/api/route53_record.tf
@@ -1,11 +1,11 @@
 resource "aws_route53_record" "main" {
-  name    = var.manually_configure_mtls_truststore ? aws_api_gateway_domain_name.main[0].regional_domain_name : aws_api_gateway_domain_name.main_nonprod[0].regional_domain_name
+  name    = aws_api_gateway_domain_name.main_nonprod[0].regional_domain_name
   type    = "A"
   zone_id = local.root_domain_id
 
   alias {
-    name    = var.manually_configure_mtls_truststore ? aws_api_gateway_domain_name.main[0].regional_domain_name : aws_api_gateway_domain_name.main_nonprod[0].regional_domain_name
-    zone_id = var.manually_configure_mtls_truststore ? aws_api_gateway_domain_name.main[0].regional_zone_id : aws_api_gateway_domain_name.main_nonprod[0].regional_zone_id
+    name    = aws_api_gateway_domain_name.main_nonprod[0].regional_domain_name
+    zone_id = aws_api_gateway_domain_name.main_nonprod[0].regional_zone_id
 
     evaluate_target_health = true
   }
