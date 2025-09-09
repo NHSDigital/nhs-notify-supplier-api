4c7e9379d894612a09a5599de4e11043d2ac1625
aidenvaines-bjss <aiden.vaines@bjss.com>
2025-09-09 08:29:58 +0000

    CCM-9336 Devcontainer fixes

--- scripts/devcontainer/postcreatecommand.sh
+++ scripts/devcontainer/postcreatecommand.sh
@@ -4,7 +4,6 @@ rm -Rf ~/.asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf;
 chmod +x ~/.asdf/asdf.sh;
 echo '. $HOME/.asdf/asdf.sh' >> ~/.zshrc
-echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
 
 echo 'export GPG_TTY=$TTY' | cat - ~/.zshrc > temp && mv temp ~/.zshrc
 
