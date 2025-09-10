420cd05e800e2f37b9a2824c3785a3fc51646318
simonlabarere <simon.labarere1@nhs.net>
2025-09-10 09:30:22 +0100

    CCM-11192: Fix unit test

--- scripts/test-data/src/__test__/helpers/create_letter_helpers.test.ts
+++ scripts/test-data/src/__test__/helpers/create_letter_helpers.test.ts
@@ -2,32 +2,34 @@ import { LetterRepository } from "../../../../../internal/datastore/src/letter-r
 import { LetterStatusType } from "../../../../../internal/datastore/src/types";
 import { createLetter } from "../../helpers/create_letter_helpers";
 import { uploadFile } from "../../helpers/s3_helpers";
-import { mockDeep } from 'jest-mock-extended';
+import { mockDeep } from "jest-mock-extended";
 
-jest.mock('../../helpers/s3_helpers')
+jest.mock("../../helpers/s3_helpers");
 
-describe('Create letter helpers', () => {
-
+describe("Create letter helpers", () => {
   beforeEach(() => {
     jest.resetAllMocks();
   });
 
-  it('create letter', async () => {
+  it("create letter", async () => {
+    jest.useFakeTimers();
+    jest.setSystemTime(new Date(2020, 1, 1));
+
     const mockPutLetter = jest.fn();
     const mockedLetterRepository = {
-      putLetter: mockPutLetter
+      putLetter: mockPutLetter,
     } as any as LetterRepository;
     const mockedUploadFile = uploadFile as jest.Mock;
 
-    const supplierId = 'supplierId';
-    const letterId = 'letterId';
-    const bucketName = 'bucketName';
-    const targetFilename = 'targetFilename';
-    const groupId = 'groupId';
-    const specificationId = 'specificationId';
-    const status = 'PENDING' as LetterStatusType;
+    const supplierId = "supplierId";
+    const letterId = "letterId";
+    const bucketName = "bucketName";
+    const targetFilename = "targetFilename";
+    const groupId = "groupId";
+    const specificationId = "specificationId";
+    const status = "PENDING" as LetterStatusType;
 
-    createLetter({
+    await createLetter({
       letterId,
       bucketName,
       supplierId,
@@ -38,7 +40,21 @@ describe('Create letter helpers', () => {
       letterRepository: mockedLetterRepository,
     });
 
-    expect(mockedUploadFile).toHaveBeenCalledWith('bucketName', 'supplierId', '../../test_letter.pdf', 'targetFilename');
-    //expect(mockPutLetter).toHaveBeenCalledWith({}); does not work
+    expect(mockedUploadFile).toHaveBeenCalledWith(
+      "bucketName",
+      "supplierId",
+      "../../test_letter.pdf",
+      "targetFilename",
+    );
+    expect(mockPutLetter).toHaveBeenCalledWith({
+      createdAt: "2020-02-01T00:00:00.000Z",
+      groupId: "groupId",
+      id: "letterId",
+      specificationId: "specificationId",
+      status: "PENDING",
+      supplierId: "supplierId",
+      updatedAt: "2020-02-01T00:00:00.000Z",
+      url: "s3://bucketName/supplierId/targetFilename",
+    });
   });
 });
