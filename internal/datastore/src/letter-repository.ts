537bd9671425171543b1ae7afe782b48875ffcd1
Mark Slowey <mark.slowey1@nhs.net>
2025-09-18 16:37:03 +0100

    CCM-11192: Batch put supplierStatusSK

--- internal/datastore/src/letter-repository.ts
+++ internal/datastore/src/letter-repository.ts
@@ -60,6 +60,7 @@ export class LetterRepository {
       lettersDb.push({
         ...letters[i],
         supplierStatus: `${letters[i].supplierId}#${letters[i].status}`,
+        supplierStatusSk: Date.now().toString(),
         ttl: Math.floor(Date.now() / 1000 + 60 * 60 * this.config.ttlHours)
       });
 
