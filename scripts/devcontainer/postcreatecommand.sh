1472bcfc233c6d995ac58efcff26df54cab2195b
Mark Slowey <mark.slowey1@nhs.net>
2025-09-26 13:22:55 +0100

    Revert "Merge remote-tracking branch 'origin/main' into feature/CCM-11188"
    This reverts commit 2b862fd07abf99c5744ffa1f5432ca85a8fed4c0, reversing
    changes made to 51675ac237d7e2770a2b4ad38eb74a139dacf383.

--- scripts/devcontainer/postcreatecommand.sh
+++ scripts/devcontainer/postcreatecommand.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+pipx install pre-commit
+
 rm -Rf ~/.asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf;
 chmod +x ~/.asdf/asdf.sh;
 echo '. $HOME/.asdf/asdf.sh' >> ~/.zshrc
+echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
 
 echo 'export GPG_TTY=$TTY' | cat - ~/.zshrc > temp && mv temp ~/.zshrc
 
