160c09f85516177330975375af7cff35ef04a672
Mark Slowey <mark.slowey1@nhs.net>
2025-10-09 15:27:25 +0100

    CCM-11207: ensure directories present

--- docs/generate-dependencies.sh
+++ docs/generate-dependencies.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 cd "$(git rev-parse --show-toplevel)"
 
+mkdir -p ./docs/_includes/components/generated
+
 # Database mermaid diagrams
 npm run -w internal/datastore diagrams
 cp ./internal/datastore/src/types.md ./docs/_includes/components/generated/types.md
