4cfbf54d5faebf619a4f8b3a582208181496ba33
Mike Houston <michael.houston4@nhs.net>
2025-10-11 12:32:30 +0000

    chore: Enhance devcontainer setup with improved Zsh configuration and additional dependencies

--- scripts/devcontainer/postcreatecommand.sh
+++ scripts/devcontainer/postcreatecommand.sh
@@ -5,21 +5,19 @@ echo 'export GPG_TTY=$TTY' | cat - ~/.zshrc > temp && mv temp ~/.zshrc
 echo 'export PATH="$HOME/go/bin:/usr/local/go/bin:$PATH"' >> ~/.zshrc
 echo 'export PATH="$HOME/.asdf/shims:$PATH"' >> ~/.zshrc
 echo 'eval "$(asdf completion zsh)"' >> ~/.zshrc
-echo 'export SSL_CERT_FILE="/etc/ssl/certs/ca-certificates.crt"' >> ~/.zshrc
-echo 'export REQUESTS_CA_BUNDLE="/etc/ssl/certs/ca-certificates.crt"' >> ~/.zshrc
-echo 'export CURL_CA_BUNDLE="/etc/ssl/certs/ca-certificates.crt"' >> ~/.zshrc
 source ~/.zshrc
 
-# Create pip config for SSL certificates before make config runs
-mkdir -p ~/.config/pip
-cat > ~/.config/pip/pip.conf << EOF
-[global]
-cert = /etc/ssl/certs/ca-certificates.crt
-EOF
+# # Create pip config for SSL certificates before make config runs
+# mkdir -p ~/.config/pip
+# cat > ~/.config/pip/pip.conf << EOF
+# [global]
+# cert = /etc/ssl/certs/ca-certificates.crt
+# EOF
 
+make _install-dependencies # required before config to ensure python is available due to race between config:: make targets
 make config
 
-gem install jekyll bundler
+sudo gem install jekyll bundler
 jekyll --version && cd docs && bundle install
 
 echo 'jekyll setup complete'
