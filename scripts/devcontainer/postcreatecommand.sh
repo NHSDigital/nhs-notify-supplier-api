da8fc382165cf3119ba66b31e83871758309d8eb
Mike Houston <michael.houston4@nhs.net>
2025-10-11 10:57:27 +0000

    chore: Update devcontainer configuration and post-create script for improved SSL certificate handling

--- scripts/devcontainer/postcreatecommand.sh
+++ scripts/devcontainer/postcreatecommand.sh
@@ -1,18 +1,25 @@
-#!/bin/bash
-
-rm -Rf ~/.asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf;
-chmod +x ~/.asdf/asdf.sh;
-echo '. $HOME/.asdf/asdf.sh' >> ~/.zshrc
+#!/bin/zsh
 
 echo 'export GPG_TTY=$TTY' | cat - ~/.zshrc > temp && mv temp ~/.zshrc
 
+echo 'export PATH="$HOME/go/bin:/usr/local/go/bin:$PATH"' >> ~/.zshrc
+echo 'export PATH="$HOME/.asdf/shims:$PATH"' >> ~/.zshrc
+echo 'eval "$(asdf completion zsh)"' >> ~/.zshrc
 source ~/.zshrc
 
-echo 'asdf setup complete'
+# Create pip config for SSL certificates before make config runs
+mkdir -p ~/.config/pip
+cat > ~/.config/pip/pip.conf << EOF
+[global]
+cert = /usr/local/share/ca-certificates/combined-cacerts.pem
+trusted-host = pypi.org
+  files.pythonhosted.org
+  pypi.python.org
+EOF
 
 make config
 
+gem install jekyll bundler
 jekyll --version && cd docs && bundle install
 
 echo 'jekyll setup complete'
