3215a53c76033b9cce6fc675401966493a5dea8a
Mike Houston <michael.houston4@nhs.net>
2025-10-22 09:44:20 +0100

    Add group and specification IDs to letter event domain model

--- internal/events/src/domain/letter.ts
+++ internal/events/src/domain/letter.ts
@@ -34,6 +34,16 @@ The identifier will be included as the origin in the subject of any correspondin
       subject: "customer/00f3b388-bbe9-41c9-9e76-052d37ee8988/letter-rendering/letter-request/0o5Fs0EELR0fUjHjbCnEtdUwQe4_0o5Fs0EELR0fUjHjbCnEtdUwQe5",
     }]
   }),
+  specificationId: z.string().meta({
+    title: "Specification ID",
+    description: "Reference to the letter specification which was used to produce a letter pack for this request.",
+    examples: ["1y3q9v1zzzz"]
+  }),
+  groupId: z.string().meta({
+    title: "Group ID",
+    description: "Identifier for the group which this letter assigned to for reporting purposes.",
+    examples: ["client_template", "00f3b388-bbe9-41c9-9e76-052d37ee8988_20a1ab22-6136-47ae-ac0f-989f382be8df"]
+  }),
   status: $LetterStatus,
   reasonCode: z.string().optional().meta({
     title: "Reason Code",
