6a95229adafc9386465e965913fa12fc5877c198
stevebux <104152898+stevebux@users.noreply.github.com>
2025-10-27 14:28:42 +0000

    Added get status endpoint for single letter (#193)
    * Added get status endpoint for single letter

--- lambdas/api-handler/src/index.ts
+++ lambdas/api-handler/src/index.ts
@@ -1,10 +1,12 @@
 import { createDependenciesContainer } from "./config/deps";
+import { createGetLetterHandler } from "./handlers/get-letter";
 import { createGetLetterDataHandler } from "./handlers/get-letter-data";
 import { createGetLettersHandler } from "./handlers/get-letters";
 import { createPatchLetterHandler } from "./handlers/patch-letter";
 
 const container = createDependenciesContainer();
 
+export const getLetter = createGetLetterHandler(container);
 export const getLetterData = createGetLetterDataHandler(container);
 export const getLetters = createGetLettersHandler(container);
 export const patchLetter = createPatchLetterHandler(container);
