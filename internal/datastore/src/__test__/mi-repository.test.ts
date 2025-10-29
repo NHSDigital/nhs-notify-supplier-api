e802a39ad83adeb14b016b3a6247ee3a9222980e
stevebux <104152898+stevebux@users.noreply.github.com>
2025-10-29 14:20:58 +0000

    CCM-11189: POST MI lambda (#199)
    * CCM-11201 Store MI in DDB
    
    * CCM-11189 Lambda to POST management information

---
+++ internal/datastore/src/__test__/mi-repository.test.ts
@@ -0,0 +1,67 @@
+import { Logger } from "pino";
+import { setupDynamoDBContainer, createTables, DBContext, deleteTables } from "./db";
+import { createTestLogger, LogStream } from "./logs";
+import { MIRepository } from "../mi-repository";
+
+// Database tests can take longer, especially with setup and teardown
+jest.setTimeout(30000);
+
+
+describe('MiRepository', () => {
+  let db: DBContext;
+  let miRepository: MIRepository;
+  let logStream: LogStream;
+  let logger: Logger;
+
+
+  beforeAll(async () => {
+    db = await setupDynamoDBContainer();
+  });
+
+  beforeEach(async () => {
+    await createTables(db);
+    (
+      { logStream, logger } = createTestLogger()
+    );
+
+    miRepository = new MIRepository(db.docClient, logger, db.config);
+  });
+
+  afterEach(async () => {
+    await deleteTables(db);
+    jest.useRealTimers();
+  });
+
+  afterAll(async () => {
+    await db.container.stop();
+  });
+
+  describe('putMi', () => {
+
+    it('creates a letter with id and timestamps', async () => {
+
+      jest.useFakeTimers();
+      // Month is zero-indexed in JS Date
+      jest.setSystemTime(new Date(2020, 1, 1));
+      const mi = {
+        specificationId: 'spec1',
+        supplierId: 'supplier1',
+        groupId:'group1',
+        lineItem: 'item1',
+        quantity: 12,
+        timestamp: new Date().toISOString(),
+        stockRemaining: 0
+      };
+
+      const persistedMi = await(miRepository.putMI(mi));
+
+      expect(persistedMi).toEqual(expect.objectContaining({
+        id: expect.any(String),
+        createdAt: '2020-02-01T00:00:00.000Z',
+        updatedAt: '2020-02-01T00:00:00.000Z',
+        ttl: 1580518800, // 2020-02-01T00:01:00.000Z, seconds since epoch
+        ...mi
+      }));
+    });
+  });
+});
--- internal/datastore/src/index.ts
+++ internal/datastore/src/index.ts
@@ -1,3 +1,4 @@
 export * from './types';
+export * from './mi-repository';
 export * from './letter-repository';
 export * from './types';
