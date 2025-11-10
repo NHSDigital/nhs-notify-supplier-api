8afa97c14032be493c63b5ed43a26b27888714d7
Francisco Videira <francisco.videira@nhs.net>
2025-11-10 16:34:17 +0000

    Fix sqs

--- infrastructure/terraform/components/api/module_sqs_letter_status_updates.tf
+++ infrastructure/terraform/components/api/module_sqs_letter_status_updates.tf
@@ -1,36 +1,14 @@
+# Queue to transport update letter status messages
 module "letter_status_updates_queue" {
   source = "https://github.com/NHSDigital/nhs-notify-shared-modules/releases/download/v2.0.24/terraform-sqs.zip"
 
   name = "letter_status_updates_queue"
-  description   = "Queue to transport update letter status messages"
 
   aws_account_id = var.aws_account_id
   component      = var.component
   environment    = var.environment
   project        = var.project
   region         = var.region
-  group          = var.group
 
   sqs_kms_key_arn = module.kms.key_arn
-
-  iam_policy_document = {
-    body = data.aws_iam_policy_document.letter_status_updates_queue.json
-  }
-}
-
-data "aws_iam_policy_document" "letter_status_updates_queue" {
-
-  statement {
-    sid    = "KMSPermissions"
-    effect = "Allow"
-
-    actions = [
-      "kms:Decrypt",
-      "kms:GenerateDataKey",
-    ]
-
-    resources = [
-      module.kms.key_arn, ## Requires shared kms module
-    ]
-  }
 }
