b0c69f29248d5712b514d8d75191ab462fae3daf
Francisco Videira <87131138+francisco-videira-nhs@users.noreply.github.com>
2025-11-18 16:07:48 +0000

    Add post letters endpoint (#231)
    * Add post letters endpoint
    
    * Fix names
    
    * Fix event mapper
    
    * Fix sqs
    
    * Fix arn
    
    * Fix sqs name
    
    * Fix param
    
    * Fix attempt
    
    * Fix attempt sqs
    
    * Fix attempt await
    
    * Clean up tests
    
    * Naming and minor refactor
    
    * Add duplicate validation
    
    * Change handler name
    
    * Fix unit tests

---
+++ lambdas/api-handler/src/handlers/post-letters.ts
@@ -0,0 +1,61 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { enqueueLetterUpdateRequests } from '../services/letter-operations';
+import { PostLettersRequest, PostLettersRequestSchema } from '../contracts/letters';
+import { ApiErrorDetail } from '../contracts/errors';
+import { ValidationError } from '../errors';
+import { processError } from '../mappers/error-mapper';
+import { assertNotEmpty, requireEnvVar, validateCommonHeaders } from '../utils/validation';
+import type { Deps } from "../config/deps";
+
+export function createPostLettersHandler(deps: Deps): APIGatewayProxyHandler {
+
+  return async (event) => {
+
+    const commonHeadersResult = validateCommonHeaders(event.headers, deps);
+
+    if (!commonHeadersResult.ok) {
+      return processError(commonHeadersResult.error, commonHeadersResult.correlationId, deps.logger);
+    }
+
+    const maxUpdateItems = requireEnvVar(deps.env, "MAX_LIMIT");
+    requireEnvVar(deps.env, "QUEUE_URL");
+
+    try {
+      const body = assertNotEmpty(event.body, new ValidationError(ApiErrorDetail.InvalidRequestMissingBody));
+
+      let postLettersRequest: PostLettersRequest;
+
+      try {
+        postLettersRequest = PostLettersRequestSchema.parse(JSON.parse(body));
+      } catch (error) {
+        if (error instanceof Error) {
+          throw new ValidationError(ApiErrorDetail.InvalidRequestBody, { cause: error});
+        }
+        else throw error;
+      }
+
+      if (postLettersRequest.data.length > maxUpdateItems) {
+        throw new ValidationError(ApiErrorDetail.InvalidRequestLettersToUpdate, { args: [maxUpdateItems]});
+      }
+
+      if( duplicateIdsExist(postLettersRequest) ) {
+        throw new ValidationError(ApiErrorDetail.InvalidRequestDuplicateLetterId);
+      }
+
+      await enqueueLetterUpdateRequests(postLettersRequest, commonHeadersResult.value.supplierId, commonHeadersResult.value.correlationId, deps);
+
+      return {
+        statusCode: 202,
+        body: ''
+      };
+
+    } catch (error) {
+      return processError(error, commonHeadersResult.value.correlationId, deps.logger);
+    }
+  };
+};
+
+function duplicateIdsExist(postLettersRequest: PostLettersRequest) {
+  const ids = postLettersRequest.data.map(item => item.id);
+  return new Set(ids).size !== ids.length;
+}
--- lambdas/api-handler/src/index.ts
+++ lambdas/api-handler/src/index.ts
@@ -3,6 +3,8 @@ import { createGetLetterHandler } from "./handlers/get-letter";
 import { createGetLetterDataHandler } from "./handlers/get-letter-data";
 import { createGetLettersHandler } from "./handlers/get-letters";
 import { createPatchLetterHandler } from "./handlers/patch-letter";
+import { createPostLettersHandler } from "./handlers/post-letters";
+import { createLetterStatusUpdateHandler } from "./handlers/letter-status-update";
 import { createPostMIHandler } from "./handlers/post-mi";
 
 const container = createDependenciesContainer();
@@ -11,4 +13,7 @@ export const getLetter = createGetLetterHandler(container);
 export const getLetterData = createGetLetterDataHandler(container);
 export const getLetters = createGetLettersHandler(container);
 export const patchLetter = createPatchLetterHandler(container);
+export const letterStatusUpdate = createLetterStatusUpdateHandler(container);
+export const postLetters = createPostLettersHandler(container);
+
 export const postMI = createPostMIHandler(container);
