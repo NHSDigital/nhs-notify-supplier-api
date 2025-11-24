2ac08832536a16992f85d48b4f3a5f05525ee4b6
Mark Slowey <mark.slowey1@nhs.net>
2025-11-24 14:24:41 +0000

    lint internal datastore

--- internal/datastore/src/mi-repository.ts
+++ internal/datastore/src/mi-repository.ts
@@ -1,38 +1,39 @@
-import {
-  DynamoDBDocumentClient,
-  PutCommand
-} from '@aws-sdk/lib-dynamodb';
-import { MI, MISchema } from './types';
-import { Logger } from 'pino';
-import { v4 as uuidv4 } from 'uuid';
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { Logger } from "pino";
+import { v4 as uuidv4 } from "uuid";
+import { MI, MISchema } from "./types";
 
 export type MIRepositoryConfig = {
-  miTableName: string,
-  miTtlHours: number
+  miTableName: string;
+  miTtlHours: number;
 };
 
 export class MIRepository {
-  constructor(readonly ddbClient: DynamoDBDocumentClient,
+  constructor(
+    readonly ddbClient: DynamoDBDocumentClient,
     readonly log: Logger,
-    readonly config: MIRepositoryConfig) {
-  }
+    readonly config: MIRepositoryConfig,
+  ) {}
 
-  async putMI(mi: Omit<MI, 'id' | 'createdAt' | 'updatedAt' | 'ttl'>): Promise<MI> {
-
+  async putMI(
+    mi: Omit<MI, "id" | "createdAt" | "updatedAt" | "ttl">,
+  ): Promise<MI> {
     const now = new Date().toISOString();
     const miDb = {
       ...mi,
       id: uuidv4(),
       createdAt: now,
       updatedAt: now,
-      ttl: Math.floor(Date.now() / 1000 + 60 * 60 * this.config.miTtlHours)
+      ttl: Math.floor(Date.now() / 1000 + 60 * 60 * this.config.miTtlHours),
     };
 
-    await this.ddbClient.send(new PutCommand({
-      TableName: this.config.miTableName,
-      Item: miDb
-    }));
+    await this.ddbClient.send(
+      new PutCommand({
+        TableName: this.config.miTableName,
+        Item: miDb,
+      }),
+    );
 
     return MISchema.parse(miDb);
   }
-};
+}
