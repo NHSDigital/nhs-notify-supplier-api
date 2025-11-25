df1089101d9532a72b29ac71e8de5b68d3d4171b
Steve Buxton <steve.buxton@nhs.net>
2025-11-25 10:20:40 +0000

    CCM-12937 Letter updates transformer lambda

---
+++ lambdas/letter-stream-forwarder/src/letter-stream-forwarder.ts
@@ -0,0 +1,30 @@
+import { LetterBase } from "@internal/datastore";
+import { DynamoDBStreamEvent, Handler, DynamoDBRecord } from "aws-lambda";
+import { PutRecordCommand } from "@aws-sdk/client-kinesis";
+import { Deps } from "./deps";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+
+export function createHandler(deps: Deps): Handler<DynamoDBStreamEvent> {
+  return async (event: DynamoDBStreamEvent): Promise<void> => {
+    deps.logger.info({description: 'Received event', event});
+    const statusChanges = event.Records
+      .filter(record => record.eventName === "MODIFY")
+      .filter(record => isChanged(record, 'status') || isChanged(record, 'reasonCode'));
+
+    for (const record of statusChanges) {
+      const newImage = record.dynamodb?.NewImage!;
+      const letter = unmarshall(newImage as any) as LetterBase;
+      await deps.kinesisClient.send(new PutRecordCommand({
+        StreamARN: deps.env.LETTER_CHANGE_STREAM_ARN,
+        PartitionKey: letter.id,
+        Data: Buffer.from(JSON.stringify(letter)),
+      }));
+    }
+  };
+
+  function isChanged(record: DynamoDBRecord, property: string): boolean {
+    const oldValue = record.dynamodb?.OldImage![property];
+    const newValue = record.dynamodb?.NewImage![property];
+      return oldValue?.S !== newValue?.S;
+  }
+}
