19f403fcd0c30200e9395740c7dde0cccdffa4e8
David Wass <david.wass2@nhs.net>
2025-11-27 14:14:02 +0000

    Unique stream name

--- infrastructure/terraform/components/api/kinesis_mi_change_stream.tf
+++ infrastructure/terraform/components/api/kinesis_mi_change_stream.tf
@@ -1,5 +1,5 @@
 resource "aws_kinesis_stream" "mi_change_stream" {
-  name             = "mi-change-stream"
+  name             = "${local.csi}-mi-change-stream"
   shard_count      = 1
   retention_period = 24
 }
