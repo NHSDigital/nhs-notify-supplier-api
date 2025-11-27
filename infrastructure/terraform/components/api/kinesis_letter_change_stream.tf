1e81bf9f118bef1edd214acaa5650971917ee671
David Wass <david.wass2@nhs.net>
2025-11-27 14:14:02 +0000

    Unique stream name

--- infrastructure/terraform/components/api/kinesis_letter_change_stream.tf
+++ infrastructure/terraform/components/api/kinesis_letter_change_stream.tf
@@ -1,5 +1,5 @@
 resource "aws_kinesis_stream" "letter_change_stream" {
-  name             = "letter-change-stream"
+  name             = "${local.csi}-letter-change-stream"
   shard_count      = 1
   retention_period = 24
 }
