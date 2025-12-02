d53313c4bb7bbacaf24e42e9ae61248fa48217f5
Steve Buxton <steve.buxton@nhs.net>
2025-12-02 15:43:36 +0000

    Address review comments

--- lambdas/letter-stream-forwarder/src/env.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-const EnvVarsSchema = z.object({
-  LETTER_CHANGE_STREAM_ARN: z.string(),
-});
-
-export type EnvVars = z.infer<typeof EnvVarsSchema>;
-
-export const envVars = EnvVarsSchema.parse(process.env);
