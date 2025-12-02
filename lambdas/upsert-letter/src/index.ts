d6602c34fa83a419b968a46a522513e74176ba13
Francisco Videira <francisco.videira@nhs.net>
2025-12-02 17:52:51 +0000

    Fix lambda handler name export

--- lambdas/upsert-letter/src/index.ts
+++ lambdas/upsert-letter/src/index.ts
@@ -3,5 +3,5 @@ import createUpsertLetterHandler from "./handler/upsert-handler";
 
 const container = createDependenciesContainer();
 
+// eslint-disable-next-line import-x/prefer-default-export
 export const upsertLetterHandler = createUpsertLetterHandler(container);
-export default upsertLetterHandler;
