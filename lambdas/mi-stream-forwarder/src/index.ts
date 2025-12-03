2d4a2a241c6b0e57e08fca24fc67d96f6c4fff9e
David Wass <david.wass2@nhs.net>
2025-12-03 10:11:01 +0000

    Remove stream forwarder and pass direct from db to kinesis

--- lambdas/mi-stream-forwarder/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { createHandler } from "./mi-stream-forwarder";
-import { createDependenciesContainer } from "./deps";
-
-const container = createDependenciesContainer();
-
-export const handler = createHandler(container);
