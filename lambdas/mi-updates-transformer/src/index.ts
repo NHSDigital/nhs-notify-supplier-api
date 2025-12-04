3e3987933693552c38cc40ce4b2eeec1ba5d5af3
David Wass <david.wass2@nhs.net>
2025-12-04 14:56:50 +0000

    Fix default export in index.ts

--- lambdas/mi-updates-transformer/src/index.ts
+++ lambdas/mi-updates-transformer/src/index.ts
@@ -3,4 +3,5 @@ import { createDependenciesContainer } from "./deps";
 
 const container = createDependenciesContainer();
 
+// eslint-disable-next-line import-x/prefer-default-export
 export const handler = createHandler(container);
