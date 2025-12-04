f46d3d6359c50d4f4aea9ecdcec9f1f47c0bb5d7
Mark Slowey <113013138+masl2@users.noreply.github.com>
2025-12-04 12:54:11 +0000

    CCM-13194:lint lambdas (#278)
    * lint authorizer
    
    * lint api handler
    
    * lint other lambdas
    
    * no use before define rule tweaks
    
    * exclude functions from before define as they are hoisted
    
    * function declarations to prove lint change
    
    Signed-off-by: Mark Slowey <mark.slowey1@nhs.net>

--- lambdas/api-handler/src/index.ts
+++ lambdas/api-handler/src/index.ts
@@ -1,12 +1,12 @@
 import { createDependenciesContainer } from "./config/deps";
-import { createGetLetterHandler } from "./handlers/get-letter";
-import { createGetLetterDataHandler } from "./handlers/get-letter-data";
-import { createGetLettersHandler } from "./handlers/get-letters";
-import { createPatchLetterHandler } from "./handlers/patch-letter";
-import { createPostLettersHandler } from "./handlers/post-letters";
-import { createLetterStatusUpdateHandler } from "./handlers/letter-status-update";
-import { createPostMIHandler } from "./handlers/post-mi";
-import { createGetStatusHandler } from "./handlers/get-status";
+import createGetLetterHandler from "./handlers/get-letter";
+import createGetLetterDataHandler from "./handlers/get-letter-data";
+import createGetLettersHandler from "./handlers/get-letters";
+import createPatchLetterHandler from "./handlers/patch-letter";
+import createPostLettersHandler from "./handlers/post-letters";
+import createLetterStatusUpdateHandler from "./handlers/letter-status-update";
+import createPostMIHandler from "./handlers/post-mi";
+import createGetStatusHandler from "./handlers/get-status";
 
 const container = createDependenciesContainer();
 
