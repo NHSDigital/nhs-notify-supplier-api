6b73bb169ba1a9a7ca2117a1e6a72ae9c5dc597d
namitha.prabhu <namitha.prabhu2@nhs.net>
2025-12-10 11:47:10 +0000

    checks for supplier data
    Signed-off-by: Mark Slowey <mark.slowey1@nhs.net>

---
+++ tests/config/global-setup.ts
@@ -0,0 +1,30 @@
+import {
+  checkSupplierExists,
+  createSupplierEntry,
+} from "../helpers/generate-fetch-test-data";
+import { SUPPLIERID } from "../constants/api-constants";
+
+async function globalSetup() {
+  console.log("");
+  console.log("*** BEGINNING GLOBAL SETUP ***");
+  console.log("");
+
+  // check supplier exists
+  const supplier = await checkSupplierExists(SUPPLIERID);
+  if (supplier) {
+    console.log(`Supplier with ID ${SUPPLIERID} already exists.`);
+    console.log("");
+    console.log("*** GLOBAL SETUP COMPLETE ***");
+    console.log("");
+    return;
+  }
+
+  console.log(`Creating supplier entry with ID: ${SUPPLIERID}`);
+  await createSupplierEntry(SUPPLIERID);
+
+  console.log("");
+  console.log("*** GLOBAL SETUP COMPLETE ***");
+  console.log("");
+}
+
+export default globalSetup;
