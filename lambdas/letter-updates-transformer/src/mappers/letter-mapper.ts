a32b9436ddcfc1f8b579ea46068e58d9b3ee0062
stevebux <104152898+stevebux@users.noreply.github.com>
2025-12-11 10:05:21 +0000

    Feature/ccm 12937- publish supplier letter events (#274)
    * CCM-12937 Letter updates transformer lambda

---
+++ lambdas/letter-updates-transformer/src/mappers/letter-mapper.ts
@@ -0,0 +1,43 @@
+import { LetterEvent } from "@nhsdigital/nhs-notify-event-schemas-supplier-api/src";
+import { randomBytes, randomUUID } from "node:crypto";
+import eventSchemaPackage from "@nhsdigital/nhs-notify-event-schemas-supplier-api/package.json";
+import { LetterForEventPub } from "../types";
+
+export default function mapLetterToCloudEvent(
+  letter: LetterForEventPub,
+): LetterEvent {
+  const eventId = randomUUID();
+  const dataschemaversion = eventSchemaPackage.version;
+  return {
+    specversion: "1.0",
+    id: eventId,
+    type: `uk.nhs.notify.supplier-api.letter.${letter.status}.v1`,
+    plane: "data",
+    dataschema: `https://notify.nhs.uk/cloudevents/schemas/supplier-api/letter.${letter.status}.${dataschemaversion}.schema.json`,
+    dataschemaversion,
+    source: "/data-plane/supplier-api/letters",
+    subject: `letter-origin/supplier-api/letter/${letter.id}`,
+
+    data: {
+      domainId: letter.id as LetterEvent["data"]["domainId"],
+      status: letter.status,
+      specificationId: letter.specificationId,
+      supplierId: letter.supplierId,
+      groupId: letter.groupId,
+      reasonCode: letter.reasonCode,
+      reasonText: letter.reasonText,
+      origin: {
+        domain: "supplier-api",
+        source: "/data-plane/supplier-api/letters",
+        subject: `letter-origin/supplier-api/letter/${letter.id}`,
+        event: eventId,
+      },
+    },
+    time: letter.updatedAt,
+    datacontenttype: "application/json",
+    traceparent: `00-${randomBytes(16).toString("hex")}-${randomBytes(8).toString("hex")}-01`,
+    recordedtime: letter.updatedAt,
+    severitynumber: 2,
+    severitytext: "INFO",
+  };
+}
