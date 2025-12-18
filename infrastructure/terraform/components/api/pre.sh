82cf3b2e89ea24b97c4ffc09e618700fb1b0aff3
Ben Hansell <ben.hansell1@nhs.net>
2025-12-18 10:53:40 +0000

    CCM-13038: LetterRequestPrepared PACT tests (#296)
    * CCM-13038: initial
    
    * CCM-13038: implement parsing on PACT consumer test
    
    * suppress leak warning for test data
    
    * CCM-13038: generate correct consumer pacts
    
    * CCM-13038: set GITHUB token when reading packages
    
    * CCM-13038: use node action
    
    * CCM-13038: use node action
    
    * CCM-13038: test publish contracts on package contract change
    
    * CCM-13038: set _authToken on gpr repo on CI
    
    * CCM-13038: pass GITHUB token to action
    
    * CCM-13038: ignore EOF lines for contracts
    
    * CCM-13038: use branch version of internal repo
    
    * CCM-13038: use script in pre.sh
    
    * CCM-13038: revert back to main
    
    * CCM-13038: redeploy
    
    * CCM-13038: update source
    
    * CCM-13038: loosen restrictions on dataschema minor and patch
    
    * CCM-13038: remove setup-github-token.sh from install action and use passed in node_version
    
    * CCM-13038: use NODE_AUTH_TOKEN for pact-test job
    
    ---------
    
    Co-authored-by: Mark Slowey <mark.slowey1@nhs.net>

--- infrastructure/terraform/components/api/pre.sh
+++ infrastructure/terraform/components/api/pre.sh
@@ -2,6 +2,16 @@
 # # It ensures all Node.js dependencies are installed, generates any required dependencies,
 # # and builds all Lambda functions in the workspace before Terraform provisions infrastructure.
 
+echo "Running Pre.sh"
+
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+
+echo "Running set-github-token.sh"
+
+$ROOT_DIR/scripts/set-github-token.sh
+
+echo "Completed."
+
 npm ci
 
 npm run generate-dependencies --workspaces --if-present
