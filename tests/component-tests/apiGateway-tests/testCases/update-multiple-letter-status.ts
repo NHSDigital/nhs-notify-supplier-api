4b1a88356a2db339711231dc0089944f996bcb7d
varsha-vanbatte-nhs <varsha.vanbatte@nhs.net>
2025-12-24 17:16:52 +0000

    comments for merge

---
+++ tests/component-tests/apiGateway-tests/testCases/update-multiple-letter-status.ts
@@ -0,0 +1,197 @@
+import { RequestHeaders } from "../../../constants/request-headers";
+import { SUPPLIERID } from "../../../constants/api-constants";
+import {
+  ErrorMessageBody,
+  PostMessageRequestBody,
+} from "../../../helpers/common-types";
+import { SupplierApiLetters } from "../../../helpers/generate-fetch-test-data";
+
+export type PostMessageResponseBody = {
+  data: {
+    type: string;
+    id: string;
+    attributes: {
+      reasonCode?: string;
+      reasonText?: string;
+      status: string;
+      specificationId: string;
+      groupId?: string;
+    };
+  };
+};
+
+export function postRequestHeaders(): RequestHeaders {
+  let requestHeaders: RequestHeaders;
+  requestHeaders = {
+    "NHSD-Supplier-ID": SUPPLIERID,
+    "NHSD-Correlation-ID": "12344",
+    "X-Request-ID": "requestId1",
+  };
+  return requestHeaders;
+}
+
+export function postInvalidRequestHeaders(): RequestHeaders {
+  let requestHeaders: RequestHeaders;
+  requestHeaders = {
+    "NHSD-Supplier-ID": SUPPLIERID,
+    "NHSD-Correlation-ID": "12344",
+    // Request Id is missing
+  };
+  return requestHeaders;
+}
+
+export function postValidRequestBody(
+  letters: SupplierApiLetters[],
+): PostMessageRequestBody {
+  let requestBody: PostMessageRequestBody;
+
+  requestBody = {
+    data: [
+      {
+        type: "Letter",
+        id: letters[0].id,
+        attributes: {
+          status: "ACCEPTED",
+        },
+      },
+      {
+        type: "Letter",
+        id: letters[1].id,
+        attributes: {
+          status: "REJECTED",
+        },
+      },
+      {
+        type: "Letter",
+        id: letters[2].id,
+        attributes: {
+          status: "PRINTED",
+        },
+      },
+      {
+        type: "Letter",
+        id: letters[3].id,
+        attributes: {
+          status: "CANCELLED",
+        },
+      },
+    ],
+  };
+  return requestBody;
+}
+
+export function postInvalidStatusRequestBody(
+  letters: SupplierApiLetters[],
+): PostMessageRequestBody {
+  let requestBody: PostMessageRequestBody;
+
+  requestBody = {
+    data: [
+      {
+        type: "Letter",
+        id: letters[0].id,
+        attributes: {
+          status: "ACCEPTED",
+        },
+      },
+      {
+        type: "Letter",
+        id: letters[1].id,
+        attributes: {
+          status: "SENDING", // Invalid letter status
+        },
+      },
+    ],
+  };
+  return requestBody;
+}
+
+export function postDuplicateIDRequestBody(
+  letters: SupplierApiLetters[],
+): PostMessageRequestBody {
+  let requestBody: PostMessageRequestBody;
+
+  requestBody = {
+    data: [
+      {
+        type: "Letter",
+        id: letters[0].id,
+        attributes: {
+          status: "ACCEPTED",
+        },
+      },
+      {
+        type: "Letter",
+        id: letters[0].id, // Duplicate id
+        attributes: {
+          status: "REJECTED",
+        },
+      },
+    ],
+  };
+  return requestBody;
+}
+
+export function postInvalidStatusResponseBody(): ErrorMessageBody {
+  let responseBody: ErrorMessageBody;
+
+  responseBody = {
+    errors: [
+      {
+        id: "12344",
+        code: "NOTIFY_INVALID_REQUEST",
+        links: {
+          about:
+            "https://digital.nhs.uk/developer/api-catalogue/nhs-notify-supplier",
+        },
+        status: "400",
+        title: "Invalid request",
+        detail: "The request body is invalid",
+      },
+    ],
+  };
+  return responseBody;
+}
+
+export function postDuplicateIDResponseBody(): ErrorMessageBody {
+  let responseBody: ErrorMessageBody;
+
+  responseBody = {
+    errors: [
+      {
+        id: "12344",
+        code: "NOTIFY_INVALID_REQUEST",
+        links: {
+          about:
+            "https://digital.nhs.uk/developer/api-catalogue/nhs-notify-supplier",
+        },
+        status: "400",
+        title: "Invalid request",
+        detail:
+          "The request cannot include multiple letter objects with the same id",
+      },
+    ],
+  };
+  return responseBody;
+}
+
+export function post500ErrorResponseBody(): ErrorMessageBody {
+  let responseBody: ErrorMessageBody;
+
+  responseBody = {
+    errors: [
+      {
+        id: "12344",
+        code: "NOTIFY_INTERNAL_SERVER_ERROR",
+        links: {
+          about:
+            "https://digital.nhs.uk/developer/api-catalogue/nhs-notify-supplier",
+        },
+        status: "500",
+        title: "Internal server error",
+        detail: "Unexpected error",
+      },
+    ],
+  };
+  return responseBody;
+}
--- tests/helpers/common-types.ts
+++ tests/helpers/common-types.ts
@@ -1,3 +1,16 @@
+export type PostMessageRequestBody = {
+  data: PostRequest[];
+};
+
+type PostRequest = {
+  type: string;
+  id: string;
+  attributes: {
+    reasonCode?: string;
+    reasonText?: string;
+    status: string;
+  };
+};
 
 export type ErrorLink = {
   about: string;
