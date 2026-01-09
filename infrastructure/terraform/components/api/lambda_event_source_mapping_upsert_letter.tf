09c4e0ee4aafcd5d16bcdea9118e4f82106adc9a
Steve Buxton <steve.buxton@nhs.net>
2026-01-09 14:15:06 +0000

    Remove batching window

--- infrastructure/terraform/components/api/lambda_event_source_mapping_upsert_letter.tf
+++ infrastructure/terraform/components/api/lambda_event_source_mapping_upsert_letter.tf
@@ -2,7 +2,6 @@ resource "aws_lambda_event_source_mapping" "upsert_letter" {
   event_source_arn                   = module.amendments_queue.sqs_queue_arn
   function_name                      = module.upsert_letter.function_name
   batch_size                         = 10
-  maximum_batching_window_in_seconds = 5
   function_response_types = [
     "ReportBatchItemFailures"
   ]
