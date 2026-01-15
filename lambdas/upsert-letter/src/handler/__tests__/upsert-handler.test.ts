ab6ded48b9ecc98db285d4ac88a17c2655c6bd08
vlasis-perdikidis <vlasis.perdikidis1@nhs.net>
2026-01-15 12:34:43 +0000

    add clean scripts in root package.json

--- lambdas/upsert-letter/src/handler/__tests__/upsert-handler.test.ts
+++ lambdas/upsert-letter/src/handler/__tests__/upsert-handler.test.ts
@@ -105,7 +105,7 @@ function createPreparedV2Event(
     type: "uk.nhs.notify.letter-rendering.letter-request.prepared.v2",
     dataschema:
       "https://notify.nhs.uk/cloudevents/schemas/letter-rendering/letter-request.prepared.2.0.0.schema.json",
-    dataschemaversion: "2.0.0",
+    dataschemaversion: "2.0.1",
   };
 }
 
