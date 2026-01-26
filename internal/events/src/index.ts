15f7b9a9c64d2455b1199633ba44e0b66451d21c
alex.nuttall1 <alex.nuttall1@nhs.net>
2026-01-26 12:21:38 +0000

    init

--- internal/events/src/index.ts
+++ internal/events/src/index.ts
@@ -4,3 +4,4 @@ export { default as DomainBase } from "./domain/domain-base";
 export * from "./events/event-envelope";
 export * from "./events/letter-events";
 export * from "./events/mi-events";
+export * from "./version"
