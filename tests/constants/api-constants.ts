f244d59f51a789b04309dffdef8c3c8e999de15c
Varsha Vanbatte <varsha.vanbatte@nhs.net>
2026-01-28 16:47:09 +0000

    Added tests for letter PDF

--- tests/constants/api-constants.ts
+++ tests/constants/api-constants.ts
@@ -9,3 +9,4 @@ export const SUPPLIERID = "TestSupplier1";
 export const MI_ENDPOINT = "mi";
 export const SUPPLIERTABLENAME = `nhs-${envName}-supapi-suppliers`;
 export const UPSERT_LETTER_LAMBDA_ARN = `arn:aws:lambda:eu-west-2:820178564574:function:nhs-${envName}-supapi-upsertletter`;
+export const DATA = "data";
