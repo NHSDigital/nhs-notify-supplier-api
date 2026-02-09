78b7a8a1732dbb0875d4a5709ef4d9ef882bb68a
Steve Buxton <steve.buxton@nhs.net>
2026-02-09 16:09:52 +0000

    Renamed queue and lambda

--- infrastructure/terraform/components/api/module_sqs_letter_status_updates.tf
+++ infrastructure/terraform/components/api/module_sqs_letter_status_updates.tf
@@ -1,9 +1,8 @@
-# Queue to transport update letter status messages. Now replaced by module.amendments_queue.
-# This queue will not be removed just yet, to allow it to be drained following the release in which module.amendments_queue replaces it.
-module "letter_status_updates_queue" {
+# Queue to transport letter status amendment messages
+module "amendments_queue" {
   source = "https://github.com/NHSDigital/nhs-notify-shared-modules/releases/download/v2.0.24/terraform-sqs.zip"
 
-  name = "letter_status_updates_queue"
+  name = "amendments_queue"
 
   aws_account_id = var.aws_account_id
   component      = var.component
