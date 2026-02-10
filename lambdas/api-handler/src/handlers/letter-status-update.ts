e84bf1c530e8ef1117eeb3aad713d8cf373e1d60
vlasis-perdikidis <vlasis.perdikidis1@nhs.net>
2026-02-10 12:42:55 +0000

    log EMF object for letter-status-update failure

--- lambdas/api-handler/src/handlers/letter-status-update.ts
+++ lambdas/api-handler/src/handlers/letter-status-update.ts
@@ -1,10 +1,13 @@
-import { SQSEvent, SQSHandler } from "aws-lambda";
+import { SQSEvent, SQSHandler, SQSRecord } from "aws-lambda";
+import { Unit } from "aws-embedded-metrics";
+import pino from "pino";
 import {
   UpdateLetterCommand,
   UpdateLetterCommandSchema,
 } from "../contracts/letters";
 import { Deps } from "../config/deps";
 import { mapToUpdateLetter } from "../mappers/letter-mapper";
+import { buildEMFObject } from "../utils/metrics";
 
 export default function createLetterStatusUpdateHandler(
   deps: Deps,
@@ -27,9 +30,21 @@ export default function createLetterStatusUpdateHandler(
           },
           "Error processing letter status update",
         );
+        // create metric object
+        emitAndFlushMetricLog(message, deps.logger);
       }
     });
 
     await Promise.all(tasks);
   };
+}
+
+function emitAndFlushMetricLog(message: SQSRecord, logger: pino.Logger) {
+  const metric = {
+    key: "statusUpdateFailed",
+    value: 1,
+    unit: Unit.Count,
+  };
+  const emf = buildEMFObject("letter-status-update", {}, metric);
+  logger.info(emf);
 }
--- lambdas/api-handler/src/utils/metrics.ts
+++ lambdas/api-handler/src/utils/metrics.ts
@@ -20,3 +20,34 @@ export enum MetricStatus {
   Success = "success",
   Failure = "failure",
 }
+
+interface MetricEntry {
+  key: string;
+  value: number;
+  unit: Unit;
+}
+
+// build EMF object
+export function buildEMFObject(
+  functionName: string,
+  dimensions: Record<string, string>,
+  metric: MetricEntry,
+) {
+  const namespace = process.env.AWS_LAMBDA_FUNCTION_NAME || functionName;
+  return {
+    LogGroup: namespace,
+    ServiceName: namespace,
+    ...dimensions,
+    _aws: {
+      Timestamp: Date.now(),
+      CloudWatchMetrics: [
+        {
+          Namespace: namespace,
+          Dimensions: [[...Object.keys(dimensions), "ServiceName", "LogGroup"]],
+          Metrics: [{ Name: metric.key, Unit: metric.value }],
+        },
+      ],
+    },
+    [metric.key]: metric.value,
+  };
+}
