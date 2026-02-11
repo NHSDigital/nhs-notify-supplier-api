ebca78582a69dabf993e24978a73dc320eac0aac
Steve Buxton <steve.buxton@nhs.net>
2026-02-11 13:45:00 +0000

    Revert "Add new amendments SNS topic"
    This reverts commit 3c854247b8f640f6cc09dcf9d6a033e5e7963b17.

--- infrastructure/terraform/modules/eventsub/sns_topic_policy.tf
+++ infrastructure/terraform/modules/eventsub/sns_topic_policy.tf
@@ -4,12 +4,6 @@ resource "aws_sns_topic_policy" "main" {
   policy = data.aws_iam_policy_document.sns_topic_policy.json
 }
 
-resource "aws_sns_topic_policy" "amendments_topic" {
-  arn = aws_sns_topic.amendments_topic.arn
-
-  policy = data.aws_iam_policy_document.amendments_topic_policy.json
-}
-
 data "aws_iam_policy_document" "sns_topic_policy" {
   policy_id = "__default_policy_ID"
 
@@ -64,64 +58,6 @@ data "aws_iam_policy_document" "sns_topic_policy" {
 
     resources = [
       aws_sns_topic.main.arn,
-    ]
-  }
-}
-
-data "aws_iam_policy_document" "amendments_topic_policy" {
-  policy_id = "__default_policy_ID"
-
-  statement {
-    sid    = "AllowAllSNSActionsFromAccount"
-    effect = "Allow"
-
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-
-    actions = [
-      "SNS:Subscribe",
-      "SNS:SetTopicAttributes",
-      "SNS:RemovePermission",
-      "SNS:Receive",
-      "SNS:Publish",
-      "SNS:ListSubscriptionsByTopic",
-      "SNS:GetTopicAttributes",
-      "SNS:DeleteTopic",
-      "SNS:AddPermission",
-    ]
-
-    resources = [
-      aws_sns_topic.eventsub_topic.arn,
-    ]
-
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceOwner"
-
-      values = [
-        var.aws_account_id,
-      ]
-    }
-  }
-
-  statement {
-    sid    = "AllowAllSNSActionsFromSharedAccount"
-    effect = "Allow"
-    actions = [
-      "SNS:Publish",
-    ]
-
-    principals {
-      type = "AWS"
-      identifiers = [
-        "arn:aws:iam::${var.shared_infra_account_id}:root"
-      ]
-    }
-
-    resources = [
-      aws_sns_topic.amendments_topic.arn,
     ]
   }
 }
