aa23c01b950c1e446783bbb6fc2687dfb1a961f5
vlasis-perdikidis <vlasis.perdikidis1@nhs.net>
2026-02-12 10:23:35 +0000

    stringify the process.env object

--- lambdas/api-handler/src/handlers/post-letters.ts
+++ lambdas/api-handler/src/handlers/post-letters.ts
@@ -41,7 +41,7 @@ function emitSuccessMetrics(
     };
     const emf = buildEMFObject("postLetters", dimensions, metric);
     logger.info(emf);
-    logger.info(`process.env: ${process.env}`);
+    logger.info(`process.env: ${JSON.stringify(process.env)}`);
   }
 }
 
