bbfa9208314fe92b083413b43d54a203bd91190f
stevebux <104152898+stevebux@users.noreply.github.com>
2026-02-16 16:02:17 +0000

    Feature/ccm 12951 Switch letter_status_update lambda to use eventsub SNS topic (#360)
    * Switch letter_status_update lambda to use eventsub SNS topic
    
    * Renamed queue and lambda
    
    
    ---------
    
    Co-authored-by: David Wass <david.wass2@nhs.net>

--- infrastructure/terraform/components/api/event_source_mapping_status_updates_to_handler.tf
+++ infrastructure/terraform/components/api/event_source_mapping_status_updates_to_handler.tf
@@ -1,12 +1,12 @@
 resource "aws_lambda_event_source_mapping" "status_updates_sqs_to_status_update_handler" {
-  event_source_arn                   = module.letter_status_updates_queue.sqs_queue_arn
-  function_name                      = module.letter_status_update.function_arn
+  event_source_arn                   = module.amendments_queue.sqs_queue_arn
+  function_name                      = module.amendment_event_transformer.function_arn
   batch_size                         = 10
   maximum_batching_window_in_seconds = 1
   scaling_config { maximum_concurrency = 10 }
 
   depends_on = [
-    module.letter_status_updates_queue, # ensures queue exists
-    module.letter_status_update         # ensures update handler exists
+    module.amendments_queue,           # ensures queue exists
+    module.amendment_event_transformer # ensures update handler exists
   ]
 }
