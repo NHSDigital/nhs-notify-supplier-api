5cf233dee4f3f35dc4de5fed36144c1580a0c469
Francisco Videira <francisco.videira@nhs.net>
2026-02-20 14:37:30 +0000

    Fix dirs

--- infrastructure/terraform/components/api/module_apigw_alarms.tf
+++ infrastructure/terraform/components/api/module_apigw_alarms.tf
@@ -3,5 +3,5 @@ module "apigw_alarms" {
   alarm_prefix = local.csi
   api_name     = aws_api_gateway_rest_api.main.name
   stage_name   = aws_api_gateway_stage.main.stage_name
-  tags         = local.default_tag
+  tags         = local.default_tags
 }
