b20e2e12a57fb917fba192d70f4a88a36cc06a15
Steve Buxton <steve.buxton@nhs.net>
2026-02-20 13:45:48 +0000

    Switch table indexes around

--- infrastructure/terraform/components/api/ddb_table_letter_queue.tf
+++ infrastructure/terraform/components/api/ddb_table_letter_queue.tf
@@ -3,7 +3,7 @@ resource "aws_dynamodb_table" "letter-queue" {
   billing_mode = "PAY_PER_REQUEST"
 
   hash_key  = "supplierId"
-  range_key = "letterId"
+  range_key = "queueTimestamp"
 
   ttl {
     attribute_name = "ttl"
@@ -12,7 +12,7 @@ resource "aws_dynamodb_table" "letter-queue" {
 
   local_secondary_index {
     name            = "timestamp-index"
-    range_key       = "queueTimestamp"
+    range_key       = "letterId"
     projection_type = "ALL"
   }
 
