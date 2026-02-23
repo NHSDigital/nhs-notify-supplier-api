651f80e3249fa119236a49459e4f91b519ed70e7
David Wass <david.wass2@nhs.net>
2026-02-23 15:46:20 +0000

    get rest of dbdata

--- internal/datastore/src/supplier-config-repository.ts
+++ internal/datastore/src/supplier-config-repository.ts
@@ -1,6 +1,17 @@
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+  QueryCommand,
+} from "@aws-sdk/lib-dynamodb";
 import { Logger } from "pino";
-import { $LetterVariant, LetterVariant } from "./SupplierConfigDomain";
+import {
+  $LetterVariant,
+  LetterVariant,
+  $VolumeGroup,
+  VolumeGroup,
+  $SupplierAllocation,
+  SupplierAllocation,
+} from "./SupplierConfigDomain";
 
 export type SupplierConfigRepositoryConfig = {
   supplierConfigTableName: string;
@@ -24,5 +35,40 @@ export class SupplierConfigRepository {
       throw new Error(`Letter variant with id ${variantId} not found`);
     }
     return $LetterVariant.parse(result.Item);
+  }
+
+  async getVolumeGroup(groupId: string): Promise<VolumeGroup> {
+    const result = await this.ddbClient.send(
+      new GetCommand({
+        TableName: this.config.supplierConfigTableName,
+        Key: { PK: "VOLUME_GROUP", SK: groupId },
+      }),
+    );
+    if (!result.Item) {
+      throw new Error(`Volume group with id ${groupId} not found`);
+    }
+    return $VolumeGroup.parse(result.Item);
+  }
+
+  async getSupplierAllocationsForVolumeGroup(
+    groupId: string,
+  ): Promise<SupplierAllocation[]> {
+    const result = await this.ddbClient.send(
+      new QueryCommand({
+        TableName: this.config.supplierConfigTableName,
+        IndexName: "VolumeGroupIndex",
+        KeyConditionExpression: "PK = :pk AND volumeGroup = :groupId",
+        ExpressionAttributeValues: {
+          ":pk": "SUPPLIER_ALLOCATIONS",
+          ":groupId": groupId,
+        },
+      }),
+    );
+    if (!result.Item) {
+      throw new Error(
+        `Supplier allocations for volume group with id ${groupId} not found`,
+      );
+    }
+    return $SupplierAllocation.array().parse(result.Item.items);
   }
 }
