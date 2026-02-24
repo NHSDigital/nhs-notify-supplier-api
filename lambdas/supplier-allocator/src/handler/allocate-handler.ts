62ce88085a4c959249d930877f943514b6f1d0d1
David Wass <david.wass2@nhs.net>
2026-02-24 14:45:29 +0000

    refactor to supplier-config services

--- lambdas/supplier-allocator/src/handler/allocate-handler.ts
+++ lambdas/supplier-allocator/src/handler/allocate-handler.ts
@@ -8,6 +8,11 @@ import {
 } from "internal/datastore/src/SupplierConfigDomain";
 import { LetterRequestPreparedEventV2 } from "@nhsdigital/nhs-notify-event-schemas-letter-rendering";
 import z from "zod";
+import {
+  getSupplierAllocationsForVolumeGroup,
+  getVariantDetails,
+  getVolumeGroupDetails,
+} from "../services/supplier-config";
 import { Deps } from "../config/deps";
 
 type SupplierSpec = { supplierId: string; specId: string };
@@ -48,120 +53,6 @@ function validateType(event: unknown) {
   ) {
     throw new Error(`Unexpected event type: ${env.data.type}`);
   }
-}
-
-async function getVariantDetails(
-  variantId: string,
-  deps: Deps,
-): Promise<LetterVariant> {
-  deps.logger.info({
-    description: "Fetching letter variant details from database",
-    variantId,
-  });
-
-  const variantDetails: LetterVariant =
-    await deps.supplierConfigRepo.getLetterVariant(variantId);
-
-  if (variantDetails) {
-    deps.logger.info({
-      description: "Fetched letter variant details",
-      variantId,
-      variantDetails,
-    });
-  } else {
-    deps.logger.error({
-      description: "No letter variant found for id",
-      variantId,
-    });
-  }
-  return variantDetails;
-}
-
-async function getVolumeGroupDetails(
-  groupId: string,
-  deps: Deps,
-): Promise<VolumeGroup> {
-  deps.logger.info({
-    description: "Fetching volume group details from database",
-    groupId,
-  });
-
-  const groupDetails = await deps.supplierConfigRepo.getVolumeGroup(groupId);
-  if (groupDetails) {
-    deps.logger.info({
-      description: "Fetched volume group details",
-      groupId,
-      groupDetails,
-    });
-  } else {
-    deps.logger.error({
-      description: "No volume group found for id",
-      groupId,
-    });
-  }
-
-  if (
-    groupDetails.status !== "PROD" &&
-    (new Date(groupDetails.startDate) > new Date() ||
-      (groupDetails.endDate && new Date(groupDetails.endDate) < new Date()))
-  ) {
-    deps.logger.error({
-      description: "Volume group is not active based on status and dates",
-      groupId,
-      status: groupDetails.status,
-      startDate: groupDetails.startDate,
-      endDate: groupDetails.endDate,
-    });
-    throw new Error(`Volume group with id ${groupId} is not active`);
-  }
-  return groupDetails;
-}
-
-async function getSupplierAllocationsForVolumeGroup(
-  groupId: string,
-  supplierId: string,
-  deps: Deps,
-): Promise<SupplierAllocation[]> {
-  deps.logger.info({
-    description: "Fetching supplier allocations for volume group from database",
-    groupId,
-  });
-  const allocations =
-    await deps.supplierConfigRepo.getSupplierAllocationsForVolumeGroup(groupId);
-
-  if (allocations.length > 0) {
-    deps.logger.info({
-      description:
-        "Fetched supplier allocations for volume group from database",
-      groupId,
-      count: allocations.length,
-    });
-  } else {
-    deps.logger.error({
-      description: "No supplier allocations found for volume group id",
-      groupId,
-    });
-  }
-
-  if (supplierId) {
-    const filteredAllocations = allocations.filter(
-      (alloc) => alloc.supplier === supplierId,
-    );
-    if (filteredAllocations.length === 0) {
-      deps.logger.error({
-        description:
-          "No supplier allocations found for variantsupplier id in volume group",
-        groupId,
-        supplierId,
-      });
-      throw new Error(
-        `No supplier allocations found for variant supplier id ${supplierId} in volume group ${groupId}`,
-      );
-    }
-    return filteredAllocations;
-  }
-
-  return allocations;
 }
 
 async function getSupplierFromConfig(letterEvent: PreparedEvents, deps: Deps) {
