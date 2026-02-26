6ab3a1b61a8a39131841804c420320e588cd78e1
David Wass <david.wass2@nhs.net>
2026-02-26 15:26:21 +0000

    new unit tests

--- internal/datastore/src/__test__/db.ts
+++ internal/datastore/src/__test__/db.ts
@@ -36,6 +36,7 @@ export async function setupDynamoDBContainer() {
     lettersTtlHours: 1,
     letterQueueTtlHours: 1,
     miTtlHours: 1,
+    supplierConfigTableName: "supplier-config",
   };
 
   return {
@@ -145,6 +146,31 @@ const createLetterQueueTableCommand = new CreateTableCommand({
     { AttributeName: "queueTimestamp", AttributeType: "S" },
   ],
 });
+const createSupplierConfigTableCommand = new CreateTableCommand({
+  TableName: "supplier-config",
+  BillingMode: "PAY_PER_REQUEST",
+  KeySchema: [
+    { AttributeName: "PK", KeyType: "HASH" }, // Partition key
+    { AttributeName: "SK", KeyType: "RANGE" }, // Sort key
+  ],
+  GlobalSecondaryIndexes: [
+    {
+      IndexName: "volumeGroup-index",
+      KeySchema: [
+        { AttributeName: "PK", KeyType: "HASH" }, // Partition key for GSI
+        { AttributeName: "volumeGroup", KeyType: "RANGE" }, // Sort key for GSI
+      ],
+      Projection: {
+        ProjectionType: "ALL",
+      },
+    },
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "PK", AttributeType: "S" },
+    { AttributeName: "SK", AttributeType: "S" },
+    { AttributeName: "volumeGroup", AttributeType: "S" },
+  ],
+});
 
 export async function createTables(context: DBContext) {
   const { ddbClient } = context;
@@ -155,6 +181,7 @@ export async function createTables(context: DBContext) {
   await ddbClient.send(createMITableCommand);
   await ddbClient.send(createSupplierTableCommand);
   await ddbClient.send(createLetterQueueTableCommand);
+  await ddbClient.send(createSupplierConfigTableCommand);
 }
 
 export async function deleteTables(context: DBContext) {
@@ -165,6 +192,7 @@ export async function deleteTables(context: DBContext) {
     "management-info",
     "suppliers",
     "letter-queue",
+    "supplier-config",
   ]) {
     await ddbClient.send(
       new DeleteTableCommand({
--- lambdas/supplier-allocator/src/services/supplier-config.ts
+++ lambdas/supplier-allocator/src/services/supplier-config.ts
@@ -10,26 +10,8 @@ export async function getVariantDetails(
   variantId: string,
   deps: Deps,
 ): Promise<LetterVariant> {
-  deps.logger.info({
-    description: "Fetching letter variant details from database",
-    variantId,
-  });
-
   const variantDetails: LetterVariant =
     await deps.supplierConfigRepo.getLetterVariant(variantId);
-
-  if (variantDetails) {
-    deps.logger.info({
-      description: "Fetched letter variant details",
-      variantId,
-      variantDetails,
-    });
-  } else {
-    deps.logger.error({
-      description: "No letter variant found for id",
-      variantId,
-    });
-  }
   return variantDetails;
 }
 
@@ -37,30 +19,13 @@ export async function getVolumeGroupDetails(
   groupId: string,
   deps: Deps,
 ): Promise<VolumeGroup> {
-  deps.logger.info({
-    description: "Fetching volume group details from database",
-    groupId,
-  });
-
   const groupDetails = await deps.supplierConfigRepo.getVolumeGroup(groupId);
-  if (groupDetails) {
-    deps.logger.info({
-      description: "Fetched volume group details",
-      groupId,
-      groupDetails,
-    });
-  } else {
-    deps.logger.error({
-      description: "No volume group found for id",
-      groupId,
-    });
-    return undefined as any;
-  }
 
   if (
-    groupDetails.status !== "PROD" ||
-    new Date(groupDetails.startDate) > new Date() ||
-    (groupDetails.endDate && new Date(groupDetails.endDate) < new Date())
+    groupDetails &&
+    (groupDetails.status !== "PROD" ||
+      new Date(groupDetails.startDate) > new Date() ||
+      (groupDetails.endDate && new Date(groupDetails.endDate) < new Date()))
   ) {
     deps.logger.error({
       description: "Volume group is not active based on status and dates",
@@ -79,29 +44,8 @@ export async function getSupplierAllocationsForVolumeGroup(
   supplierId: string,
   deps: Deps,
 ): Promise<SupplierAllocation[]> {
-  deps.logger.info({
-    description: "Fetching supplier allocations for volume group from database",
-    groupId,
-  });
   const allocations =
     await deps.supplierConfigRepo.getSupplierAllocationsForVolumeGroup(groupId);
-
-  if (allocations.length > 0) {
-    deps.logger.info({
-      description:
-        "Fetched supplier allocations for volume group from database",
-      groupId,
-      count: allocations.length,
-    });
-  } else {
-    deps.logger.error({
-      description: "No supplier allocations found for volume group id",
-      groupId,
-    });
-    throw new Error(
-      `No supplier allocations found for volume group id ${groupId}`,
-    );
-  }
 
   if (supplierId) {
     const filteredAllocations = allocations.filter(
@@ -132,12 +76,6 @@ export async function getSupplierDetails(
 
   const supplierDetails: Supplier[] =
     await deps.supplierConfigRepo.getSuppliersDetails(supplierIds);
-
-  deps.logger.info({
-    description: "Fetched supplier details for supplier allocations",
-    supplierIds,
-    supplierDetails,
-  });
 
   if (Object.keys(supplierDetails).length === 0) {
     deps.logger.error({
