b4c5f0bfff483210b79d8256ec54e9bb7266c5cc
Steve Buxton <steve.buxton@nhs.net>
2026-03-11 14:47:22 +0000

    Rename visibilityTimeout to visibilityTimestamp

--- internal/datastore/src/letter-queue-repository.ts
+++ internal/datastore/src/letter-queue-repository.ts
@@ -33,7 +33,7 @@ export default class LetterQueueRepository {
     const pendingLetter: PendingLetter = {
       ...insertPendingLetter,
       queueTimestamp: now,
-      visibilityTimeout: now,
+      visibilityTimestamp: now,
       ttl: Math.floor(
         Date.now() / 1000 + 60 * 60 * this.config.letterQueueTtlHours,
       ),
