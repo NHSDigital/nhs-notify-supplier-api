9aaeec8da58c3a720b4dc66af720b42f0a0533d1
ross.faulds2 <ross.faulds2@nhs.net>
2026-04-07 15:14:02 +0100

    add back in data property

--- lambdas/api-handler/src/mappers/mi-mapper.ts
+++ lambdas/api-handler/src/mappers/mi-mapper.ts
@@ -37,15 +37,17 @@ export function mapToPostMIResponse(mi: MIBase): PostMIResponse {
 
 export function mapToGetMIResponse(mi: MIBase): GetMIResponse {
   return GetMIResponseResourceSchema.parse({
-    id: mi.id,
-    type: "ManagementInformation",
-    attributes: {
-      lineItem: mi.lineItem,
-      timestamp: mi.timestamp,
-      quantity: mi.quantity,
-      specificationId: mi.specificationId,
-      groupId: mi.groupId,
-      stockRemaining: mi.stockRemaining,
+    data: {
+      id: mi.id,
+      type: "ManagementInformation",
+      attributes: {
+        lineItem: mi.lineItem,
+        timestamp: mi.timestamp,
+        quantity: mi.quantity,
+        specificationId: mi.specificationId,
+        groupId: mi.groupId,
+        stockRemaining: mi.stockRemaining,
+      },
     },
   });
 }
