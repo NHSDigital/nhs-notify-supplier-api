b0e8ac60bded118430f2ef9472ab2c9bd3395818
ross.faulds2 <ross.faulds2@nhs.net>
2026-04-08 16:28:08 +0000

    more linting

--- lambdas/api-handler/src/handlers/get-mi.ts
+++ lambdas/api-handler/src/handlers/get-mi.ts
@@ -44,19 +44,31 @@ export default function createGetMIHandler(deps: Deps): APIGatewayProxyHandler {
       });
 
       // metric with count 1 specifying the supplier
-      const dimensions: Record<string, string> = { supplierId: supplierId };
+      const dimensions: Record<string, string> = { supplierId };
       emitMetric("getMi", dimensions, deps.logger, MetricStatus.Success, 1);
 
       // metric displaying the type/number of lineItems posted per supplier
       dimensions.lineItem = result.data.attributes.lineItem;
-      emitMetric("getMi", dimensions, deps.logger, "LineItem per supplier", result.data.attributes.quantity);
+      emitMetric(
+        "getMi",
+        dimensions,
+        deps.logger,
+        "LineItem per supplier",
+        result.data.attributes.quantity,
+      );
 
       return {
         statusCode: 200,
         body: JSON.stringify(result, null, 2),
       };
     } catch (error) {
-      emitMetric("getMi", { supplierId: supplierId }, deps.logger, MetricStatus.Failure, 1);
+      emitMetric(
+        "getMi",
+        { supplierId },
+        deps.logger,
+        MetricStatus.Failure,
+        1,
+      );
       return processError(error, commonIds.value.correlationId, deps.logger);
     }
   };
