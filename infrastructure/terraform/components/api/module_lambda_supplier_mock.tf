577b85118aa5cd7b635177039e0f18c45e2a538c
vlasis-perdikidis <vlasis.perdikidis1@nhs.net>
2026-04-15 10:37:23 +0000

    correct lambda handler name

--- infrastructure/terraform/components/api/module_lambda_supplier_mock.tf
+++ infrastructure/terraform/components/api/module_lambda_supplier_mock.tf
@@ -22,7 +22,7 @@ module "supplier_mock" {
   function_code_base_path = local.aws_lambda_functions_dir_path
   function_code_dir       = "supplier-mock/dist"
   function_include_common = true
-  handler_function_name   = "supplierMockHandler"
+  handler_function_name   = "handler"
   runtime                 = "nodejs22.x"
   memory                  = 512
   timeout                 = 29
