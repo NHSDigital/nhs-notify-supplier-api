5929d29a96f3deefbe0e0222cd0146050d976d02
David Wass <david.wass2@nhs.net>
2026-04-17 14:25:03 +0100

    store current allocations

--- lambdas/supplier-allocator/src/handler/allocate-handler.ts
+++ lambdas/supplier-allocator/src/handler/allocate-handler.ts
@@ -28,7 +28,10 @@ import {
   getVariantDetails,
   getVolumeGroupDetails,
 } from "../services/supplier-config";
-import { calculateSupplierAllocatedFactor } from "../services/supplier-quotas";
+import {
+  calculateSupplierAllocatedFactor,
+  updateSupplierAllocation,
+} from "../services/supplier-quotas";
 import { Deps } from "../config/deps";
 
 type SupplierSpec = {
@@ -37,6 +40,12 @@ type SupplierSpec = {
   priority: number;
   billingId: string;
 };
+
+type SupplierDetails = {
+  supplierSpec: SupplierSpec;
+  volumeGroupId: string;
+};
+
 type PreparedEvents = LetterRequestPreparedEventV2 | LetterRequestPreparedEvent;
 
 // small envelope that must exist in all inputs
@@ -76,7 +85,10 @@ function validateType(event: unknown) {
   }
 }
 
-async function getSupplierFromConfig(letterEvent: PreparedEvents, deps: Deps) {
+async function getSupplierFromConfig(
+  letterEvent: PreparedEvents,
+  deps: Deps,
+): Promise<SupplierDetails | undefined> {
   try {
     const variantDetails: LetterVariant = await getVariantDetails(
       letterEvent.data.letterVariantId,
@@ -124,7 +136,7 @@ async function getSupplierFromConfig(letterEvent: PreparedEvents, deps: Deps) {
 
     let supplierAllocationsForPack: SupplierAllocation[] = [];
     let supplierFactors: { supplierId: string; factor: number }[] = [];
-
+    let selectedSupplierId = "unknown"; // Default to first supplier if no allocations or factors can be calculated
     if (suppliersForPack && suppliersForPack.length > 0) {
       supplierAllocationsForPack = supplierAllocations.filter((alloc) =>
         suppliersForPack.some((supplier) => supplier.id === alloc.supplier),
@@ -142,6 +154,16 @@ async function getSupplierFromConfig(letterEvent: PreparedEvents, deps: Deps) {
       console.log("Supplier factors calculated for allocation", {
         supplierFactors,
       });
+
+      // Get the supplierid with the lowest factor
+      selectedSupplierId = supplierFactors[0].supplierId;
+      let lowestFactor = supplierFactors[0].factor;
+      for (const supplierFactor of supplierFactors) {
+        if (supplierFactor.factor < lowestFactor) {
+          lowestFactor = supplierFactor.factor;
+          selectedSupplierId = supplierFactor.supplierId;
+        }
+      }
     }
 
     deps.logger.info({
@@ -157,16 +179,26 @@ async function getSupplierFromConfig(letterEvent: PreparedEvents, deps: Deps) {
       suppliersForPack,
       supplierAllocationsForPack,
       supplierFactors,
+      selectedSupplierId,
     });
 
-    return allocatedSuppliers;
+    const supplierDetails: SupplierDetails = {
+      supplierSpec: {
+        supplierId: selectedSupplierId,
+        specId: preferredPack.id,
+        priority: 0,
+        billingId: preferredPack.billingId,
+      },
+      volumeGroupId: volumeGroupDetails.id,
+    };
+    return supplierDetails;
   } catch (error) {
     deps.logger.error({
       description: "Error fetching supplier from config",
       err: error,
       variantId: letterEvent.data.letterVariantId,
     });
-    return [];
+    return undefined;
   }
 }
 
@@ -175,6 +207,7 @@ function getSupplier(letterEvent: PreparedEvents, deps: Deps): SupplierSpec {
 }
 
 type AllocationMetrics = Map<string, Map<string, number>>;
+type VolumeGroupAllocation = Map<string, Record<string, number>>;
 
 function incrementMetric(
   map: AllocationMetrics,
@@ -231,12 +264,40 @@ function emitDataMetrics(
   deps.logger.info(buildEMFObject(namespace, dimensions, metric));
 }
 
+function incrementAllocation(
+  map: VolumeGroupAllocation,
+  volumeGroupId: string,
+  supplierId: string,
+  allocation: number,
+) {
+  const groupAllocations = map.get(volumeGroupId) ?? {};
+  groupAllocations[supplierId] =
+    (groupAllocations[supplierId] ?? 0) + allocation;
+  map.set(volumeGroupId, groupAllocations);
+}
+
+async function saveAllocations(
+  deps: Deps,
+  volumeGroupAllocations: VolumeGroupAllocation,
+) {
+  for (const [volumeGroupId, allocations] of volumeGroupAllocations) {
+    for (const [supplierId, allocation] of Object.entries(allocations)) {
+      await updateSupplierAllocation(
+        volumeGroupId,
+        supplierId,
+        allocation,
+        deps,
+      );
+    }
+  }
+}
+
 export default function createSupplierAllocatorHandler(deps: Deps): SQSHandler {
   return async (event: SQSEvent) => {
     const batchItemFailures: SQSBatchItemFailure[] = [];
     const perAllocationSuccess: AllocationMetrics = new Map();
     const perAllocationFailure: AllocationMetrics = new Map();
-
+    const volumeGroupAllocations: VolumeGroupAllocation = new Map(); // Map of volume group id to supplier allocations for that group, used to track the allocations calculated in this batch for emitting metrics and updating the quotas after processing the batch
     // Initialise the supplier quotas.
 
     const tasks = event.Records.map(async (record) => {
@@ -254,8 +315,18 @@ export default function createSupplierAllocatorHandler(deps: Deps): SQSHandler {
 
         validateType(letterEvent);
 
-        const supplierSpec = getSupplier(letterEvent, deps);
-        await getSupplierFromConfig(letterEvent, deps);
+        const supplierSpec = getSupplier(letterEvent as PreparedEvents, deps);
+        const supplierDetails = await getSupplierFromConfig(
+          letterEvent as PreparedEvents,
+          deps,
+        );
+
+        incrementAllocation(
+          volumeGroupAllocations,
+          supplierDetails?.volumeGroupId ?? "unknown",
+          supplierSpec.supplierId,
+          1,
+        );
 
         supplier = supplierSpec.supplierId;
         priority = String(supplierSpec.priority);
@@ -307,6 +378,7 @@ export default function createSupplierAllocatorHandler(deps: Deps): SQSHandler {
 
     emitMetrics(perAllocationSuccess, MetricStatus.Success, deps);
     emitMetrics(perAllocationFailure, MetricStatus.Failure, deps);
+    await saveAllocations(deps, volumeGroupAllocations);
     return { batchItemFailures };
   };
 }
--- lambdas/supplier-allocator/src/services/supplier-quotas.ts
+++ lambdas/supplier-allocator/src/services/supplier-quotas.ts
@@ -1,4 +1,4 @@
-import { OverallAllocation } from "@internal/datastore";
+import { DailyAllocation, OverallAllocation } from "@internal/datastore";
 import { SupplierAllocation } from "@nhsdigital/nhs-notify-event-schemas-supplier-config";
 import { Deps } from "../config/deps";
 
@@ -33,34 +33,54 @@ export async function calculateSupplierAllocatedFactor(
   });
 }
 
-export async function updateSupplierQuota(
-  groupId: string,
+// function to either update or create a new overall allocation and daily allocation for a given supplier, volume group and allocation amount
+// if the overall allocation for the volume group does not exist, it will be created with the new allocation for the supplier and 0 for the other suppliers
+
+export async function updateSupplierAllocation(
+  volumeGroupId: string,
   supplierId: string,
   newAllocation: number,
   deps: Deps,
 ): Promise<void> {
   const overallAllocation =
-    await deps.supplierQuotasRepo.getOverallAllocation(groupId);
-
-  const updatedAllocations = overallAllocation
-    ? {
-        ...overallAllocation.allocations,
+    await deps.supplierQuotasRepo.getOverallAllocation(volumeGroupId);
+  if (overallAllocation) {
+    await deps.supplierQuotasRepo.updateOverallAllocation(
+      volumeGroupId,
+      supplierId,
+      newAllocation,
+    );
+  } else {
+    const newOverallAllocation: OverallAllocation = {
+      id: volumeGroupId,
+      volumeGroup: volumeGroupId,
+      allocations: {
         [supplierId]: newAllocation,
-      }
-    : {
+      },
+    };
+    await deps.supplierQuotasRepo.putOverallAllocation(newOverallAllocation);
+  }
+  const dailyAllocationDate = new Date().toISOString().split("T")[0]; // Get current date in YYYY-MM-DD format
+  const dailyAllocation = await deps.supplierQuotasRepo.getDailyAllocation(
+    volumeGroupId,
+    dailyAllocationDate,
+  );
+  if (dailyAllocation) {
+    await deps.supplierQuotasRepo.updateDailyAllocation(
+      volumeGroupId,
+      dailyAllocationDate,
+      supplierId,
+      newAllocation,
+    );
+  } else {
+    const newDailyAllocation: DailyAllocation = {
+      id: `${volumeGroupId}#DATE#${dailyAllocationDate}`,
+      date: dailyAllocationDate,
+      volumeGroup: volumeGroupId,
+      allocations: {
         [supplierId]: newAllocation,
-      };
-
-  const updatedOverallAllocation: OverallAllocation = overallAllocation
-    ? {
-        ...overallAllocation,
-        allocations: updatedAllocations,
-      }
-    : {
-        id: groupId,
-        volumeGroup: groupId,
-        allocations: updatedAllocations,
-      };
-
-  await deps.supplierQuotasRepo.putOverallAllocation(updatedOverallAllocation);
+      },
+    };
+    await deps.supplierQuotasRepo.putDailyAllocation(newDailyAllocation);
+  }
 }
