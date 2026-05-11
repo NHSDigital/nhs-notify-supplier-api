d17e1533657f149a650a7f6e7a6497836ac6462b
jamesthompson26-nhs <james.thompson26@nhs.net>
2026-05-11 10:46:43 +0100

    CCM-14783:SQS Module Update to Include Alarms 3.1.4

--- infrastructure/terraform/components/api/module_sqs_alarms.tf
+++ infrastructure/terraform/components/api/module_sqs_alarms.tf
@@ -2,7 +2,7 @@ module "sqs_alarms" {
   for_each = local.alarms_enabled ? local.sqs_alarm_targets : {}
   source   = "../../modules/alarms-sqs"
 
-  alarm_prefix   = local.csi
-  queue_name     = each.value
-  tags           = local.default_tags
+  alarm_prefix = local.csi
+  queue_name   = each.value
+  tags         = local.default_tags
 }
