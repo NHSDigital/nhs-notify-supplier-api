5cf4f9d9b5723406ab9e8ce55dd508ebc8ae6416
Vlasis-Perdikidis <vlasis.perdikidis1@nhs.net>
2026-05-12 19:25:29 +0100

    add new line in group-id.ts

--- internal/helpers/src/group-id.ts
+++ internal/helpers/src/group-id.ts
@@ -2,4 +2,4 @@ export function formatGroupId(clientId: string, campaignId?: string, templateId?
   const safeCampaignId = campaignId || "unknown";
   const safeTemplateId = templateId || "unknown";
   return `${clientId}_${safeCampaignId}_${safeTemplateId}`;
-}   
+}
