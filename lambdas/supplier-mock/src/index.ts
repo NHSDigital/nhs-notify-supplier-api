979d0f82d8fe6e26c5ad41b238a6be35e1c87bd1
Vlasis-Perdikidis <vlasis.perdikidis1@nhs.net>
2026-05-19 13:52:51 +0000

    export handler correctly?

--- lambdas/supplier-mock/src/index.ts
+++ lambdas/supplier-mock/src/index.ts
@@ -3,8 +3,9 @@ import createHandler from "./supplier-mock";
 
 const containerPromise = createDependenciesContainer();
 
-export default async function handler(
+// eslint-disable-next-line import-x/prefer-default-export
+export const supplierMockHandler = async (
   ...args: Parameters<ReturnType<typeof createHandler>>
-) {
+) => {
   return createHandler(await containerPromise)(...args);
-}
+};
