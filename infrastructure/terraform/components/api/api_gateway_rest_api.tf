e2475837338732c78502d202df56c4beeda329ad
Steve Buxton <steve.buxton@nhs.net>
2026-07-10 15:09:40 +0100

    Add security_policy and endpoint_access_node to aws_api_gateway_rest_api

--- infrastructure/terraform/components/api/api_gateway_rest_api.tf
+++ infrastructure/terraform/components/api/api_gateway_rest_api.tf
@@ -1,6 +1,13 @@
+locals {
+  rest_api_security_policy      = "SecurityPolicy_TLS12_PFS_2025_EDGE"
+  rest_api_endpoint_access_mode = "STRICT"
+}
+
 resource "aws_api_gateway_rest_api" "main" {
   name                         = local.csi
   body                         = local.openapi_spec
   description                  = "Suppliers API"
   disable_execute_api_endpoint = var.disable_gateway_execute_endpoint
+  security_policy              = local.rest_api_security_policy
+  endpoint_access_mode         = local.rest_api_endpoint_access_mode
 }
