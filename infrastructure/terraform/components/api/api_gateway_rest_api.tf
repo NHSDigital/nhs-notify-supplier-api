fe00794fdbcf5b6bdabe48299c7a2e3385dcf8ca
Steve Buxton <steve.buxton@nhs.net>
2026-07-13 16:28:43 +0100

    Specify regional endpoint

--- infrastructure/terraform/components/api/api_gateway_rest_api.tf
+++ infrastructure/terraform/components/api/api_gateway_rest_api.tf
@@ -6,4 +6,8 @@ resource "aws_api_gateway_rest_api" "main" {
   disable_execute_api_endpoint = var.disable_gateway_execute_endpoint
   security_policy              = "SecurityPolicy_TLS13_1_2_2021_06"
   endpoint_access_mode         = "BASIC"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
 }
