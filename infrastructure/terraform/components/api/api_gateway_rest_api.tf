4f28f6e2df311a80b34bafe611802bbc81f86c35
Steve Buxton <steve.buxton@nhs.net>
2026-07-13 14:16:00 +0100

    Force recreation

--- infrastructure/terraform/components/api/api_gateway_rest_api.tf
+++ infrastructure/terraform/components/api/api_gateway_rest_api.tf
@@ -1,7 +1,16 @@
+locals {
+  rest_api_security_policy = "TLS_1_2"
+}
+
+resource "terraform_data" "rest_api_security_policy" {
+  input = {
+    security_policy = local.rest_api_security_policy
+  }
+}
 resource "aws_api_gateway_rest_api" "main" {
   name                         = local.csi
   body                         = local.openapi_spec
   description                  = "Suppliers API"
   disable_execute_api_endpoint = var.disable_gateway_execute_endpoint
-  security_policy              = "TLS_1_2"
+  security_policy              = local.rest_api_security_policy
 }
