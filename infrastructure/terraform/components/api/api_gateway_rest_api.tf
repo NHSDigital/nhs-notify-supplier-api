59a5324e73ff638c58ddda95eeb0aabf91e0e5b3
Steve Buxton <steve.buxton@nhs.net>
2026-07-13 15:04:50 +0100

    Try enhanced security policy

--- infrastructure/terraform/components/api/api_gateway_rest_api.tf
+++ infrastructure/terraform/components/api/api_gateway_rest_api.tf
@@ -1,20 +1,9 @@
-locals {
-  rest_api_security_policy = "TLS_1_2"
-}
 
-resource "terraform_data" "rest_api_security_policy" {
-  input = {
-    security_policy = local.rest_api_security_policy
-  }
-}
 resource "aws_api_gateway_rest_api" "main" {
   name                         = local.csi
   body                         = local.openapi_spec
   description                  = "Suppliers API"
   disable_execute_api_endpoint = var.disable_gateway_execute_endpoint
-  security_policy              = local.rest_api_security_policy
-
-  lifecycle {
-    replace_triggered_by = [terraform_data.rest_api_security_policy]
-  }
+  security_policy              = "SecurityPolicy_TLS13_1_2_2021_06"
+  endpoint_access_mode         = "STRICT"
 }
