7d9687475a4764f9f77315b1c586ab1c041dc4b1
Steve Buxton <steve.buxton@nhs.net>
2026-07-13 15:41:36 +0100

    Try BASIC

--- infrastructure/terraform/components/api/api_gateway_rest_api.tf
+++ infrastructure/terraform/components/api/api_gateway_rest_api.tf
@@ -5,5 +5,5 @@ resource "aws_api_gateway_rest_api" "main" {
   description                  = "Suppliers API"
   disable_execute_api_endpoint = var.disable_gateway_execute_endpoint
   security_policy              = "SecurityPolicy_TLS13_1_2_2021_06"
-  endpoint_access_mode         = "STRICT"
+  endpoint_access_mode         = "BASIC"
 }
