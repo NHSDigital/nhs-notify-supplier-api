a16bd2871876ef6b9abe314655ff87804fcf48cf
Francisco Videira <francisco.videira@nhs.net>
2026-07-14 11:54:33 +0000

    Temp 2: CI unblock fix (squashed) from origin/feature/CCM-21851-Missing-Pack-Specification

--- infrastructure/terraform/components/api/api_gateway_rest_api.tf
+++ infrastructure/terraform/components/api/api_gateway_rest_api.tf
@@ -4,6 +4,6 @@ resource "aws_api_gateway_rest_api" "main" {
   body                         = local.openapi_spec
   description                  = "Suppliers API"
   disable_execute_api_endpoint = var.disable_gateway_execute_endpoint
-  security_policy              = "SecurityPolicy_TLS13_1_2_2021_06"
-  endpoint_access_mode         = "BASIC"
+  security_policy              = "SecurityPolicy_TLS12_PFS_2025_EDGE"
+  endpoint_access_mode         = "STRICT"
 }
