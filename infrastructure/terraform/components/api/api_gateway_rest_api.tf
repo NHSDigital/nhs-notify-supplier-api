a7decef9d9ff89984f0542b9c11727c41f127149
Steve Buxton <steve.buxton@nhs.net>
2026-07-14 14:01:10 +0100

    Try again

--- infrastructure/terraform/components/api/api_gateway_rest_api.tf
+++ infrastructure/terraform/components/api/api_gateway_rest_api.tf
@@ -1,6 +1,13 @@
 
+resource "terraform_data" "api_gateway_endpoint_settings" {
+  input = {
+    endpoint_access_mode = local.api_gateway_endpoint_access_mode
+    endpoint_type        = local.api_gateway_endpoint_type
+  }
+}
+
 resource "aws_api_gateway_rest_api" "main" {
-  name                         = "${local.csi}-${local.api_gateway_endpoint_replacement_suffix}"
+  name                         = local.csi
   body                         = local.openapi_spec
   description                  = "Suppliers API"
   disable_execute_api_endpoint = var.disable_gateway_execute_endpoint
@@ -13,5 +20,6 @@ resource "aws_api_gateway_rest_api" "main" {
 
   lifecycle {
     create_before_destroy = true
+    replace_triggered_by  = [terraform_data.api_gateway_endpoint_settings]
   }
 }
