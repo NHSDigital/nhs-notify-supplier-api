69e9bd710c7b20f69b9b335b1d481d52a84dbc4b
stevebux <104152898+stevebux@users.noreply.github.com>
2026-07-15 10:07:40 +0100

    CCM-21851: Add missing pack specification to dev config (#640)
    Also fix Terraform error

--- infrastructure/terraform/components/api/api_gateway_rest_api.tf
+++ infrastructure/terraform/components/api/api_gateway_rest_api.tf
@@ -3,8 +3,6 @@ resource "aws_api_gateway_rest_api" "main" {
   body                         = local.openapi_spec
   description                  = "Suppliers API"
   disable_execute_api_endpoint = var.disable_gateway_execute_endpoint
-
-  lifecycle {
-    replace_triggered_by = [terraform_data.rest_api_security_policy]
-  }
+  security_policy              = "SecurityPolicy_TLS12_PFS_2025_EDGE"
+  endpoint_access_mode         = "STRICT"
 }
