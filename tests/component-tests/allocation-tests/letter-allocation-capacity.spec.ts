51d7065223511c26e1910a4b96f47a47f793fdcd
Mark Slowey <mark.slowey1@nhs.net>
2026-07-21 13:21:05 +0100

    log all found logs

--- tests/component-tests/allocation-tests/letter-allocation-capacity.spec.ts
+++ tests/component-tests/allocation-tests/letter-allocation-capacity.spec.ts
@@ -67,7 +67,6 @@ test.describe("Allocator Lambda Tests", () => {
     const supplierAllocatorLog = await getAllocationLog(
       "Pack specification filtered out based on pageCount constraints",
     );
-    logger.info(`Log found: ${JSON.stringify(supplierAllocatorLog)}`);
     const filteredPackSpecId = supplierAllocatorLog.packSpecId;
     logger.info(`Pack spec filtered out ${filteredPackSpecId}`);
     expect(filteredPackSpecId).toBe("notify-c5");
