1d609e5784a23943b3780e87316194c030eb2f5d
Steve Buxton <steve.buxton@nhs.net>
2026-08-10 14:54:47 +0100

    Clean up imports  Please enter the commit message for your changes. Lines starting

--- tests/component-tests/allocation-tests/letter-allocation-rejected.spec.ts
+++ tests/component-tests/allocation-tests/letter-allocation-rejected.spec.ts
@@ -3,7 +3,6 @@ import test, { expect } from "playwright/test";
 import {
   PackErrorLog,
   getAllocationLog,
-  getAllocationLogForDomainId,
   getVariantsForAllocation,
   getVolumeGroupData,
   updateLetterVariantPackSpecs,
