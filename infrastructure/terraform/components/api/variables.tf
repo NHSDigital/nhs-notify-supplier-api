3aab7a5014116da2e05d1a906077f3835901b5bf
ross.faulds2 <ross.faulds2@nhs.net>
2026-08-27 12:55:45 +0100

    add temporarily enable only the letter queue alarams for testing

--- infrastructure/terraform/components/api/variables.tf
+++ infrastructure/terraform/components/api/variables.tf
@@ -226,6 +226,13 @@ variable "enable_alarms" {
   description = "Enable CloudWatch alarms for this deployed environment"
   default     = true
 }
+
+variable "test_alarms_enabled" {
+  type        = bool
+  description = "Enable letter queue alarms for testing when account-wide alarms are disabled"
+  default     = true
+}
+
 variable "deploy_supplier_mock_scheduler" {
   type        = bool
   description = "Deploy EventBridge Scheduler trigger for supplier mock lambda"
