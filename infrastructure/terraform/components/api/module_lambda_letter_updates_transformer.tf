b53c3b982a6e76c1bc77682b4741e545c2800741
David Wass <david.wass2@nhs.net>
2026-09-17 15:40:57 +0100

    letter updates transformer

--- infrastructure/terraform/components/api/module_lambda_letter_updates_transformer.tf
+++ infrastructure/terraform/components/api/module_lambda_letter_updates_transformer.tf
@@ -37,7 +37,7 @@ module "letter_updates_transformer" {
   lambda_env_vars = merge(local.common_lambda_env_vars, {
     EVENTPUB_SNS_TOPIC_ARN = "${module.eventpub.sns_topic.arn}",
     EVENT_SOURCE           = var.letter_event_source
-    LOG_REFERENCE          = "LETTER_UPDATES_TRANSFORMER"
+    LOG_REFERENCE          = "LTR_UPD_TFRM"
   })
 }
 
